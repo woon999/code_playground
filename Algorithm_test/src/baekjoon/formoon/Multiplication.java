a61af733a861f103b052296caba1f4d2918f3e62
loosie <jong9712@naver.com>
2020-04-11 19:01:22 +0900

    formoon end

---
+++ Algorithm_test/src/baekjoon/formoon/Multiplication.java
@@ -0,0 +1,17 @@
+package baekjoon.formoon;
+
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		
+		for(int i = 1; i< 10; i++) {
+			System.out.println(n + " * " + i +" = " + n*i);
+		}
+	}
+}
