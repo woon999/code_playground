e2cdb5719ff81ee942f72bf2cb12e88b6ccefd55
loosie <54282927+loosie@users.noreply.github.com>
2020-04-12 16:06:22 +0900

    whilemoon end

---
+++ Algorithm_test/src/baekjoon/whilemoon/AplusBFive.java
@@ -0,0 +1,19 @@
+package baekjoon.whilemoon;
+
+import java.util.Scanner;
+
+public class AplusBFive {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+
+		while (true) {
+			int a = s.nextInt(), b = s.nextInt();
+			if (a == 0 && b == 0)
+				break;
+			System.out.println(a + b);
+
+		}
+	}
+}
---
+++ Algorithm_test/src/baekjoon/whilemoon/AplusBFour.java
@@ -0,0 +1,20 @@
+package baekjoon.whilemoon;
+
+import java.util.Scanner;
+
+public class AplusBFour {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		
+		while(s.hasNextInt()) {
+			
+			int a = s.nextInt(), b = s.nextInt();
+			
+			System.out.println(a+b);
+		}
+
+	}
+
+}
