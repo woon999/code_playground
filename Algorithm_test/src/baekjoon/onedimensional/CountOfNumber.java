dad525b58e239af5a16ae15503df1099c0e9726b
loosie <54282927+loosie@users.noreply.github.com>
2020-04-14 01:30:26 +0900

    onedimensional#1

---
+++ Algorithm_test/src/baekjoon/onedimensional/CountOfNumber.java
@@ -0,0 +1,29 @@
+package baekjoon.onedimensional;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CountOfNumber {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int mul = 1;
+		for (int i = 0; i < 3; i++) {
+			int a = Integer.parseInt(br.readLine());
+			mul *= a;
+		}
+
+		int[] check = new int[10];
+		
+		
+		while (mul > 0) {
+			check[mul % 10] += 1;
+			mul /= 10;
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(check[i]);
+		}
+	}
+}
