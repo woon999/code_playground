b8c64ec7fa8baeb9f9e7b5a1b5967de4950abfac
loosie <54282927+loosie@users.noreply.github.com>
2020-04-13 21:06:30 +0900

    practice#1 end

---
+++ Algorithm_test/src/baekjoon/pratice/AverageScore.java
@@ -0,0 +1,25 @@
+package baekjoon.pratice;
+
+import java.util.Scanner;
+
+public class AverageScore {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int[] arr = new int[5];
+		int total = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = s.nextInt();
+			if (arr[i] < 40) {
+				arr[i] = 40;
+			}
+			total += arr[i];
+		}
+
+		int average = total / arr.length;
+
+		System.out.println(average);
+
+	}
+}
