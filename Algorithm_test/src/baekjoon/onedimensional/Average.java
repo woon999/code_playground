ff85a8927d6ec1dc14fe8ce8cbe70a25458d7a21
loosie <jong9712@naver.com>
2020-04-14 20:57:37 +0900

    onedimensional end

---
+++ Algorithm_test/src/baekjoon/onedimensional/Average.java
@@ -0,0 +1,31 @@
+package baekjoon.onedimensional;
+
+import java.io.*;
+import java.util.*;
+
+public class Average {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		double[] arr = new double[n];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i =0; i<n; i++) {
+			arr[i]  = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+
+		double average =0;
+		for(int i = 0; i<n; i++) {
+			arr[i] = (arr[i]/arr[n-1])*100;
+			average += arr[i];
+		}
+		
+		System.out.println(average/n);
+		
+	}
+}
---
+++ Algorithm_test/src/baekjoon/onedimensional/OverAverage.java
@@ -0,0 +1,44 @@
+package baekjoon.onedimensional;
+
+import java.io.*;
+import java.util.*;
+
+public class OverAverage {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int c = Integer.parseInt(br.readLine());
+
+//		double[] percent = new double[c];
+		
+		for (int i = 0; i < c; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int[] student = new int[Integer.parseInt(st.nextToken())];
+
+			double average = 0;
+			for (int j = 0; j < student.length; j++) {
+				student[j] = Integer.parseInt(st.nextToken());
+				average += student[j];
+			}
+			
+			average /= student.length;
+
+			double count = 0;
+			for (int k = 0; k < student.length; k++) {
+				if (student[k] > average)
+					count++;
+			}
+
+			double result = (count / student.length) * 100;
+			System.out.println(String.format("%.3f", result) + "%");
+//			for (int l = 0; l < c; l++) {
+//				percent[i] = result;
+//			}
+
+		}
+
+//		for (int i = 0; i < c; i++)
+//			System.out.println(String.format("%.3f", percent[i]) + "%");
+
+	}
+}
