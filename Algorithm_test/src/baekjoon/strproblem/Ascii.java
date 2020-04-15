0ea195429d2de3ccc92ff5eb27c2a7900e7297be
loosie <54282927+loosie@users.noreply.github.com>
2020-04-15 22:49:21 +0900

    str문제 #1

---
+++ Algorithm_test/src/baekjoon/strproblem/Ascii.java
@@ -0,0 +1,15 @@
+package baekjoon.strproblem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Ascii {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		char c = s.charAt(0);
+		
+		System.out.println((int)c);
+	}
+}
