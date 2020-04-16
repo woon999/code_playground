0db5fa77f1acd9901a7270d7c1f9252c0f839060
loosie <54282927+loosie@users.noreply.github.com>
2020-04-16 21:56:01 +0900

    strproblem#2

---
+++ Algorithm_test/src/baekjoon/strproblem/NumberOfWord.java
@@ -0,0 +1,20 @@
+package baekjoon.strproblem;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NumberOfWord {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] s = br.readLine().split(" ");
+
+		int count = s.length;
+		for (int i = 0; i < s.length; i++) {
+			if (s[i].isEmpty()) {
+				count--;
+			}	
+		}
+		System.out.println(count);
+	}
+}
