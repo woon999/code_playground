726759d13807a9e133120a99e0690902b0b4dd49
loosie <jong9712@naver.com>
2020-05-02 21:44:18 +0900

    sort#2 (nsort3 recheck)

---
+++ Algorithm_test/src/baekjoon/sort/NumberSort2.java
@@ -0,0 +1,31 @@
+package baekjoon.sort;
+
+import java.io.*;
+import java.util.*;
+
+public class NumberSort2 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+
+		ArrayList<Integer> num = new ArrayList<Integer>();
+
+		for (int i = 0; i < n; i++) {
+			num.add(Integer.parseInt(br.readLine()));
+		}
+
+		Collections.sort(num);
+
+		for (int i = 0; i < n; i++) {
+			bw.write(num.get(i)+"\n");
+			
+		}
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+}
