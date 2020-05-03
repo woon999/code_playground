0de6ff9c3e68d206b87299c4111bc8e00e56af98
loosie <jong9712@naver.com>
2020-05-03 19:50:11 +0900

    recheck

--- Algorithm_test/src/baekjoon/sort/NumberSort3.java
+++ Algorithm_test/src/baekjoon/sort/NumberSort3.java
@@ -7,34 +7,57 @@ import java.util.*;
 public class NumberSort3 {
 
 	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
-
-		// index가 해당 숫자, index의 value: index가 나온 횟수
-		int[] count_arr = new int[10001]; // 나온 숫자의 개수 count
-		int check = 0;
-
-		// 숫자 입력받기
-		for (int i = 0; i < n; i++) {
-			int temp = Integer.parseInt(br.readLine());
-			count_arr[temp] += 1; // 숫자가 나온 횟수 저장
-		}
-
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		for (int i = 1; i <= 10000; i++) {
-			for (int j = 0; j < count_arr[i]; j++) {
-				check++;
-				bw.write(Integer.toString(i) + "\n");
-				// 모든 수를 출력했는데 for문 돌고 있을 경우 방지
-				if (check == n)
-					break;
-			}
-		}
-
-		br.close();
-		bw.close();
+        int n = Integer.parseInt(br.readLine());
+        int[] count = new int[10000];
+        int check = 0;
+        
+        for(int i=0 ; i<n ; i++)
+            count[Integer.parseInt(br.readLine())-1]++; 
+        
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=0 ; i<10000 ; i++) {
+            for(int j=0 ; j<count[i] ; j++) {
+            	check++;
+                bw.write(Integer.toString(i+1) + "\n");
+                
+                if(check == n)break;
+            }
+        }
+        bw.close();
+        br.close();        
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//		int n = Integer.parseInt(br.readLine());
+//
+//		// index가 해당 숫자, index의 value: index가 나온 횟수
+//		int[] count_arr = new int[10001]; // 나온 숫자의 개수 count
+//		int check = 0;
+//
+//		// 숫자 입력받기
+//		for (int i = 0; i < n; i++) {
+//			int temp = Integer.parseInt(br.readLine());
+//			count_arr[temp] += 1; // 숫자가 나온 횟수 저장
+//		}
+//
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//		for (int i = 1; i <= 10000; i++) {
+//			for (int j = 0; j < count_arr[i]; j++) {
+//				check++;
+//				bw.write(Integer.toString(i) + "\n");
+//				// 모든 수를 출력했는데 for문 돌고 있을 경우 방지
+//				if (check == n)
+//					break;
+//			}
+//		}
+//
+//		br.close();
+//		bw.close();
+		
+//		////////////////////////////////////////////////////////////////
 
 //		Integer[] num = new Integer[n];
 //		for (int i = 0; i < n; i++) {
