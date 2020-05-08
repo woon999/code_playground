f61870f2b9fcfd2b9c06361da555e2b9b36cef9a
loosie <54282927+loosie@users.noreply.github.com>
2020-05-09 00:32:05 +0900

    kakao winter #3 다시풀기★★★

--- Algorithm_test/src/kakao/winter/BannedId.java
+++
@@ -1,9 +0,0 @@
-package kakao.winter;
-
-public class BannedId {
-	
-	 public static int solution(String[] user_id, String[] banned_id) {
-	        int answer = 0;
-	        return answer;
-	    }
-}
---
+++ Algorithm_test/src/kakao/winter/Kakao04.java
@@ -0,0 +1,18 @@
+package kakao.winter;
+
+
+//	2019 카카오 개발자 겨울 인턴십 코딩테스트 #4 호텔 방 배정
+//	[본 문제는 정확성과 효율성 테스트 각각 점수가 있는 문제입니다.]
+
+// 	      입출력 예시
+//		k	room_number		result
+//		10	[1,3,4,1,3,1]	[1,3,4,2,5,6]
+public class Kakao04 {
+	
+	
+    public static long[] solution(long k, long[] room_number) {
+        long[] answer = {};
+        return answer;
+    }
+
+}
