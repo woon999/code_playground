8bad090457511f0c4c11ac9f20639efc3f76f034
loosie <jong9712@naver.com>
2020-06-04 17:56:30 +0900

    maththree #4

---
+++ Algorithm_test/src/baekjoon/maththree/BinomialCoefficient.java
@@ -0,0 +1,25 @@
+package baekjoon.maththree;
+
+// #11050
+import java.util.Scanner;
+
+public class BinomialCoefficient {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		sc.close();
+		System.out.println(factoiral(n)/(factoiral(m)*factoiral(n-m)));
+		
+	}
+	
+	static int factoiral(int n) {
+		if(n<=1)
+			return 1;
+		
+		return n*factoiral(n-1);
+	}
+}
