b1ebd218e0d30d7d354b5aa66c7ae7981f46b8fa
loosie <jong9712@naver.com>
2020-06-07 18:07:23 +0900

    stack #3

---
+++ Algorithm_test/src/baekjoon/stack/Bracket.java
@@ -0,0 +1,48 @@
+package baekjoon.stack;
+
+// #9012
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Bracket {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String s = st.nextToken();
+			
+			boolean vps = true;
+			Stack<Character> stack = new Stack<Character>();
+			
+			for (int j = 0; j < s.length(); j++) {
+				if (s.charAt(j) == '(') {
+					stack.push(s.charAt(j));
+				}
+				else if(s.charAt(j)==')') {
+					if(!stack.isEmpty()) {
+						stack.pop();
+					}else {
+						vps =false;
+						break;
+					}
+				}
+
+			}
+			if(!stack.isEmpty()) {
+				vps = false;
+			}
+			
+			if(vps) {
+				System.out.println("YES");
+			}else {
+				System.out.println("NO");
+			}
+		}
+	}
+}
