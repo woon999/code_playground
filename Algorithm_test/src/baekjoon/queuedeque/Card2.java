61c416fd2d392d94b117476be208f79559249aee
loosie <jong9712@naver.com>
2020-06-10 14:52:26 +0900

    queuedeque #2

---
+++ Algorithm_test/src/baekjoon/queuedeque/Card2.java
@@ -0,0 +1,30 @@
+package baekjoon.queuedeque;
+
+// #2146
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Card2 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		for(int i =1; i<n+1; i++) {
+			q.offer(i);
+		}
+		
+		while(q.size()>1) {
+			q.poll();
+			int a = q.poll();
+			q.offer(a);
+		}
+		
+		System.out.println(q.poll());
+	}
+}
