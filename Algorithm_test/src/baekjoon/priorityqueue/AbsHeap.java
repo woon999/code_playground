e283bcd5f0f7ed22e71091b5c88eb23ed59cb95a
loosie <jong9712@naver.com>
2020-07-08 16:34:04 +0900

    priorityqueue end

---
+++ Algorithm_test/src/baekjoon/priorityqueue/AbsHeap.java
@@ -0,0 +1,43 @@
+package baekjoon.priorityqueue;
+
+// #11286
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class AbsHeap {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new PriorityQueue<Integer>((o1,o2)->{
+			int abs1 = Math.abs(o1);
+			int abs2 = Math.abs(o2);
+
+			if(abs1 == abs2) return o1 >o2? 1 : -1;
+			return abs1 -abs2;
+		});
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num ==0) {
+				if(!q.isEmpty()) {
+					sb.append(q.poll()+"\n");
+				}else {
+					sb.append(0+"\n");
+				}
+			}
+			else {
+				q.add(num);
+			}
+
+//			System.out.println(q);
+		}
+		System.out.println(sb);
+		
+	}
+}
