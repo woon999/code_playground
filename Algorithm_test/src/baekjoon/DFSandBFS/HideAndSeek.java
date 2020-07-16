bc0a400af6848d7c40720dbd7232de03bbbe9e61
loosie <jong9712@naver.com>
2020-07-16 18:18:55 +0900

    DFSandBFS #end (★CrashWall)
    CrashWall
    벽을 1번만 뚫어야 하는데 boolean으로 제어를 하게되면 제어를 하기 전에 먼저 스택에 쌓인 큐들로 인해 오류 발생  따라서, int형으로 바꾼다음에 모든 값을 Max로 초기화 하고 벽을 한번 뚫으면 1 , 뚫린 길은 0으로 표시하여 중복 코스 방지와 한번 벽을 뚫으면 다른 벽은 뚫을 수 없도록
     설정한다.         if (checked[nx][ny] > p.chance)로 제어한다. //p.chance 초기값 0 , checeked배열에 뚫린길은  0 , 벽은 1의 값을 대입

---
+++ Algorithm_test/src/baekjoon/DFSandBFS/HideAndSeek.java
@@ -0,0 +1,48 @@
+package baekjoon.DFSandBFS;
+
+// #1697
+import java.io.*;
+import java.util.*;
+
+public class HideAndSeek {
+
+	static int n, k;
+	static int[] pos;
+	static int[] dx = { -1, 1, 2 };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+
+		pos = new int[100001];
+		bfs();
+
+		System.out.println(pos[k]);
+
+	}
+
+	static void bfs() {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(n);
+		int nPos = n;
+		while (!q.isEmpty() && nPos != k) {
+			nPos = q.poll();
+			for (int i = 0; i < 3; i++) {
+				int nx;
+				if (i == 2) {
+					nx = nPos * dx[i];
+				} else {
+					nx = nPos + dx[i];
+				}
+				
+				if (nx >= 0 && nx < 100001 && pos[nx] == 0) {
+					q.add(nx);
+					pos[nx] = pos[nPos] + 1;
+				}
+			}
+		}
+	}
+}
