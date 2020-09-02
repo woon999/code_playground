269a3c37f8959d5a6c9e6d6019fd56efc1fc9346
jongwon <54282927+loosie@users.noreply.github.com>
2020-09-02 13:55:33 +0900

    greedy #26 (2847.Game)

---
+++ Algorithm_test/src/baekjoon/greedy/Game.java
@@ -0,0 +1,32 @@
+package baekjoon.greedy;
+
+// 2847
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Game {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		int count = 0;
+
+		
+			for (int i = n - 2; i >= 0; i--) {
+				if (arr[i] >= arr[i + 1]) {
+					count += arr[i] - arr[i + 1] + 1;
+					arr[i] = arr[i + 1] - 1;
+				}
+			}
+			System.out.println(count);
+		}
+
+	
+}
