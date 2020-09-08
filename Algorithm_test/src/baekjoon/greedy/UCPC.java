444813beaa22b6a8086a725b411ed54091f3a197
jongwon <54282927+loosie@users.noreply.github.com>
2020-09-08 10:08:08 +0900

    greedy #32 (15904.UCPC)

---
+++ Algorithm_test/src/baekjoon/greedy/UCPC.java
@@ -0,0 +1,31 @@
+package baekjoon.greedy;
+
+// #15904
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class UCPC {
+
+	static char[] ucpc = { 'U', 'C', 'P', 'C' };
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String s = br.readLine();
+
+		int len = 4;
+		int idx = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (idx < len) {
+				if (s.charAt(i) == ucpc[idx])
+					idx++;
+			}
+
+		}
+
+		if (len == idx)
+			System.out.println("I love UCPC");
+		else
+			System.out.println("I hate UCPC");
+	}
+}
