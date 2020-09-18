fbd235037439e892db6bd86cbe175e5c039d4508
jongwon <54282927+loosie@users.noreply.github.com>
2020-09-18 16:02:02 +0900

    greedy #42 (14916.Change2)

--- Algorithm_test/src/baekjoon/greedy/Polyomino.java
+++ Algorithm_test/src/baekjoon/greedy/Polyomino.java
@@ -5,48 +5,46 @@ import java.io.*;
 
 public class Polyomino {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String s = br.readLine();
-		
-		
-		String result ="";
-		
-		int count =0;
-		for(int i=0; i<s.length();) {
-			
-			if(s.charAt(i) == '.') {
+
+		String result = "";
+
+		int count = 0;
+		for (int i = 0; i < s.length();) {
+
+			if (s.charAt(i) == '.') {
 				i++;
 				result += '.';
 				continue;
 			}
-			
-			for(int j=i; j<s.length() && s.charAt(j) == 'X' ;j++) {
+
+			for (int j = i; j < s.length() && s.charAt(j)=='X'; j++) {
 				count++;
 			}
-			
+
 			i += count;
-			
-			
-			if(count %2 !=0) {
+
+			if (count % 2 != 0) {
 				System.out.println(-1);
 				return;
 			}
-			
-			while(true) {
-				if(count >=4) {
-					result+="AAAA";
-					count -=4;
-				}else if(count ==2) {
+
+			while (true) {
+				if (count >= 4) {
+					result += "AAAA";
+					count -= 4;
+				} else if (count == 2) {
 					result += "BB";
-					count -=2;
-				}else {
+					count -= 2;
+				} else {
 					break;
 				}
 			}
 		}
-		
+
 		System.out.println(result);
 	}
 }
