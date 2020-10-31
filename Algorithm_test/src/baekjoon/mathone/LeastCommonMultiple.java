981be4d87402ed1dc9f5368b51f379b5a3ec4bf9
jongwon <54282927+loosie@users.noreply.github.com>
2020-10-31 15:22:31 +0900

    mathone #11 (1934.LeastCommonMultiple)

---
+++ Algorithm_test/src/baekjoon/mathone/LeastCommonMultiple.java
@@ -0,0 +1,37 @@
+package baekjoon.mathone;
+
+// #1934
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LeastCommonMultiple {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		
+		for(int i=0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			long n1 = Long.parseLong(st.nextToken());
+			long n2 = Long.parseLong(st.nextToken());
+
+			long gcd = getGCD(Math.max(n1, n2), Math.min(n1, n2));
+			System.out.println(n1*n2/gcd);
+		}
+		
+		
+	}	
+	
+	public static long getGCD(long a, long b) {
+		while(b>0) {
+			long tmp = a;
+			a = b;
+			b = tmp%b;
+		}
+		
+		return a;
+	}
+}
