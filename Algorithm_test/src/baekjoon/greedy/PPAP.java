f4dc26b134bff19cfe4be90a9b4b6f6faa656f7b
jongwon <54282927+loosie@users.noreply.github.com>
2020-11-09 15:04:59 +0900

    greedy #69 (16120.PPAP)

---
+++ Algorithm_test/src/baekjoon/greedy/PPAP.java
@@ -0,0 +1,39 @@
+package baekjoon.greedy;
+
+// #16120
+import java.io.*;
+import java.util.Stack;
+
+public class PPAP {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String text = br.readLine();
+		Stack<Character> s = new Stack<>();
+
+		for (int i = 0; i < text.length(); i++) {
+			char tmp = text.charAt(i);
+
+			if (tmp == 'P')
+				s.push('P');
+
+			else {
+				if (s.size() > 1 && i < text.length() - 1 && text.charAt(i + 1) == 'P') {
+					s.pop();
+					s.pop();
+				} else {
+					System.out.println("NP");
+					return;
+				}
+			}
+
+		}
+
+		if (s.size() == 1) {
+			System.out.println("PPAP");
+		} else {
+			System.out.println("NP");
+		}
+	}
+}
