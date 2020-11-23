f6fcbc5dbae9a1cb8b68349a301061e211f0785e
jongwon <54282927+loosie@users.noreply.github.com>
2020-11-23 13:55:06 +0900

    dynamicplanning3 #16 (2133. GeneralTiling)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/GeneralTiling.java
@@ -0,0 +1,36 @@
+package baekjoon.dynamicplanning3;
+
+// #2133
+import java.io.*;
+
+public class GeneralTiling {
+	
+	static int[] dp;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		int n = Integer.parseInt(br.readLine());
+		
+		dp = new int[n+1];
+		
+		int answer =0;
+		
+		System.out.println(n%2 == 0 ? solve(n) : 0);
+		
+		
+	}
+	
+	private static int solve(int n) {
+		if(n ==0) return 1;
+		if(n ==2) dp[2] = 3;
+		else if (dp[n] == 0) {
+			for(int i=2; i<n+1; i+=2) {
+				int idx = i ==2 ? 3:2;
+				dp[n] += idx * solve(n-i);
+			}
+		}
+		
+		return dp[n];
+	}
+}
