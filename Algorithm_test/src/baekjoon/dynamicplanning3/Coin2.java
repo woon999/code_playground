41805615f6bd9c63ec1559a5bf8bde09a909bce7
jongwon <54282927+loosie@users.noreply.github.com>
2020-11-29 14:32:40 +0900

    dynamicplanning3 #22 (2294.Coin2)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/Coin2.java
@@ -0,0 +1,37 @@
+package baekjoon.dynamicplanning3;
+
+// #2294 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Coin2 {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		int[] coin = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] dp= new int[k+1];
+		Arrays.fill(dp, 100001);
+		dp[0] = 0;
+		
+		for(int i=0; i<n; i++) {
+			for(int j= coin[i]; j< k+1; j++) {
+				dp[j] = Math.min(dp[j], dp[j - coin[i]] +1);
+			}
+		}
+		
+		System.out.println(dp[k] == 100001 ? -1 : dp[k]);
+		
+	}
+}
