c77480b10d8bf55854dc0c92c8be7ef68a1c08b7
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-05 13:22:32 +0900

    dynamicplanning3 #27 #28 (5557.FirstGrade /  11660. SumOfIntervals)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/SumOfIntervals.java
@@ -0,0 +1,53 @@
+package baekjoon.dynamicplanning3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SumOfIntervals {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] arr = new int[n+1][n+1];
+		int[][] dp = new int[n+1][n+1];
+		
+		int[] sum = new int[m];
+		
+		
+		for(int i=1; i<n+1 ;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=1; j<n+1; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i=1; i<n+1 ;i++) {
+			for(int j=1; j<n+1; j++) {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+			}
+		}
+		
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			sum[i] += dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+			
+		}
+		
+		
+		for(int i=0; i<m; i++) {
+			System.out.println(sum[i]);
+		}
+		
+	}
+}
