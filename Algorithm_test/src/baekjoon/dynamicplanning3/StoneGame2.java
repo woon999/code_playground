37363323768de811206b6e00dabde12f40f90f80
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-08 12:50:15 +0900

    dynamicplanning3 #31 (9656.StoneGame2)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/StoneGame2.java
@@ -0,0 +1,15 @@
+package baekjoon.dynamicplanning3;
+
+// #9656 
+import java.io.*;
+
+public class StoneGame2 {
+ 
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println((n%2 == 1 )?  "CY" : "SK");
+	}
+}
