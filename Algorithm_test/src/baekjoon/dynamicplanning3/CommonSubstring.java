1356bf19d59289f635233c899da3fb7fbfc9d7b1
jongwon <54282927+loosie@users.noreply.github.com>
2020-12-24 16:20:48 +0900

    dynamicplanning3 #47 (5582.CommonSubstring)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/CommonSubstring.java
@@ -0,0 +1,31 @@
+package baekjoon.dynamicplanning3;
+
+// #5582
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CommonSubstring {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		
+		int res=0;
+		int[][] dp = new int[s1.length()+1][s2.length()+1];
+		
+		for(int i=1; i<s1.length() +1; i++) {
+			for(int j=1; j<s2.length() +1; j++) {
+				if(s1.charAt(i-1) == s2.charAt(j-1)) {
+					dp[i][j] = dp[i-1][j-1] +1;
+					
+					res = (dp[i][j] > res)? dp[i][j] : res;
+				}
+			}
+		}
+		
+		
+		System.out.println(res);
+	}
+}
