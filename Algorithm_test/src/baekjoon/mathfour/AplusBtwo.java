cd0f20d01e8ae9bce61e59cdb38b50a4807e94ba
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-01 14:19:27 +0900

    mathfour #8,9 (2558.AplusBtwo, 13458.Viewer)

---
+++ Algorithm_test/src/baekjoon/mathfour/AplusBtwo.java
@@ -0,0 +1,17 @@
+package baekjoon.mathfour;
+
+// #2558
+import java.util.Scanner;
+
+public class AplusBtwo {
+
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt() + sc.nextInt();
+		
+		System.out.println(num);
+		
+		
+	}
+}
