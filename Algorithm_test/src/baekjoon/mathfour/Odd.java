e65aa6673539c977a8176605069a76460a9453b7
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-04 11:44:03 +0900

    mathfour #12 (2576.Odd)

---
+++ Algorithm_test/src/baekjoon/mathfour/Odd.java
@@ -0,0 +1,34 @@
+package baekjoon.mathfour;
+
+// #2576
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Odd {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int res=0;
+		int min = Integer.MAX_VALUE;
+		for(int i=0; i<7; i++) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n%2 !=0 ) {
+				res += n;
+				min = (min < n) ? min : n;
+			}
+			
+			
+			
+			
+		}
+		
+		if(res ==0) {
+			System.out.println(-1);
+		}else {
+		System.out.println(res);
+		System.out.println(min);
+		}
+	}
+}
+
