4a1c2c736a8ea14ba05c4992ca25c2abeec6c2b8
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-08 17:04:25 +0900

    dynamicplanning3 #52 (13301. Tile)

---
+++ Algorithm_test/src/baekjoon/dynamicplanning3/Tile.java
@@ -0,0 +1,31 @@
+package baekjoon.dynamicplanning3;
+
+
+// #13301
+import java.io.*;
+
+public class Tile {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		long[] dp = new long[81];
+		long[] ddp = new long[81];
+ 		dp[1] = 1;
+		dp[2] = 1;
+		
+		for(int i=3; i<81; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		ddp[1] = 4;
+		for(int i=2; i<81; i++) {
+			ddp[i] = ddp[i-1] + dp[i]*2;
+		}
+		
+		System.out.println(ddp[n]);
+		
+	}
+}
