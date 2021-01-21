0e67c905fd18b28cf7ee0fa4695d41c5a1c6dfbd
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-21 22:06:38 +0900

    mathfour #20 (1550.Hexadecimal)

---
+++ Algorithm_test/src/baekjoon/mathfour/Hexadecimal.java
@@ -0,0 +1,16 @@
+package baekjoon.mathfour;
+
+// #1550 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Hexadecimal {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = br.readLine();
+		
+		System.out.println(Integer.parseInt(a, 16));
+	}
+}
