bcb200493a7398773291b9ea5c2e7adb3ca820a0
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-25 14:04:33 +0900

    Stack #7 (10799.Pipe)

---
+++ Algorithm_test/src/baekjoon/stack/Pipe.java
@@ -0,0 +1,35 @@
+package baekjoon.stack;
+
+// #10799
+import java.io.*;
+import java.util.Stack;
+
+public class Pipe {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> st = new Stack<>();
+
+		String s = br.readLine();
+		int count =0;
+		
+		for(int i=0; i<s.length(); i++) {
+			if(s.substring(i, i+1).equals("(")) {
+				st.push(i);
+			}
+			else {
+				if(st.peek()==i-1) {
+					st.pop();
+					count+=st.size();
+				}
+				else {
+					st.pop();
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+		
+		
+	}
+}
