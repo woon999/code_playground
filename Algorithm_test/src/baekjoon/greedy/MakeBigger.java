e6dfb6d9aa02fb91ba17592b8b759b30f928a962
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-27 17:40:51 +0900

    greedy update (2812.MakeBigger)

--- Algorithm_test/src/baekjoon/greedy/MakeBigger.java
+++ Algorithm_test/src/baekjoon/greedy/MakeBigger.java
@@ -3,6 +3,7 @@ package baekjoon.greedy;
 // #2812
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class MakeBigger {
@@ -14,31 +15,46 @@ public class MakeBigger {
 		
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		
 		String s = br.readLine();
-		char[] num = new char[n-k];
-		int max = 0;
 		
-		for(int i=0; i<n; i++) {
+		Stack<Character> stack = new Stack<>();
+		
+		stack.push(s.charAt(0));
+		int cnt = 0;
+		boolean flag = true;
+		
+		for(int i=1; i<s.length(); i++) {
+			char ch = s.charAt(i);
 
-			if(max == n-k) break;
+			while(!stack.isEmpty() && flag) {
+				if(cnt == k) {
+					flag = false;
+					break;
+				}
+				if(stack.peek() < ch) {
+					stack.pop();
+					cnt ++;
+				}
+				
+				else if(stack.peek() >= ch) {
+					break;
+				}
 			
-			while(max >0 && k>0 && num[max-1] < s.charAt(i)) {
-				max--;
-				k--;
 			}
-			
-			num[max++] = s.charAt(i);
-			
+			stack.push(ch);			
 		}
 		
-		for(int i=0; i<num.length; i++) {
-			sb.append(num[i]);
+		if(cnt < k) {
+			int gap = k-cnt;
+			for(int i=0; i<stack.size()-gap; i++) {
+				sb.append(stack.get(i));
+			}
+		}else {
+			for(int i=0; i<stack.size(); i++) {
+				sb.append(stack.get(i));
+			}
 		}
-		
-		
-		
-		
+	
 		System.out.println(sb.toString());
 	}
 }
