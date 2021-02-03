4134c7d83b7186953f83f33f96fd63e58001ec53
jongwon <54282927+loosie@users.noreply.github.com>
2021-02-03 14:58:14 +0900

    mathfour#29 (2845.AfterParty)

---
+++ Algorithm_test/src/baekjoon/mathfour/AfterParty.java
@@ -0,0 +1,31 @@
+package baekjoon.mathfour;
+
+// #2845
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AfterParty {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int l = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<5; i++) {
+			int news = Integer.parseInt(st.nextToken());
+			sb.append(news-l*p + " ");	
+		}
+		
+		
+		System.out.println(sb.toString());
+		
+	}
+}
