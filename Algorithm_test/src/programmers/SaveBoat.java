115e18acabf1a1f838c2da8d4f4e2ea0f36d76f5
jongwon <54282927+loosie@users.noreply.github.com>
2021-02-24 17:03:53 +0900

    programmers #21 그리디 구명보트

---
+++ Algorithm_test/src/programmers/SaveBoat.java
@@ -0,0 +1,40 @@
+package programmers;
+
+// 그리디 #4 구명보트 
+import java.util.*;
+
+public class SaveBoat {
+
+	public static void main(String[] args) {
+		int[] people = {70, 50, 80, 50};
+//		int[] people = {70, 30, 80, 20};
+		int limit = 100;
+		
+		System.out.println(solution(people, limit));
+	}
+	
+	public static int solution(int[] people, int limit) {
+        int answer = 0;
+
+        Arrays.sort(people);
+        
+        for(int i : people) {
+        	System.out.println(i);
+        }
+        int idx=0;
+        
+        for(int j=people.length-1; j>=idx; j--) {
+        	int sum =people[idx] + people[j];
+        	
+        	if(limit >= sum) {
+        		System.out.println(sum);
+        		answer ++;
+        		idx++;
+        	}else {
+        		answer++;
+        	}
+        }
+        
+        return answer;
+    }
+}
