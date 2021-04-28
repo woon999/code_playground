d251a194a95693c6cf764585423fe48886dce813
jongwon <54282927+loosie@users.noreply.github.com>
2021-04-28 15:21:04 +0900

    bj graph (2644. 촌수계산) dfs

---
+++ Algorithm_test/src/baekjoon/ttone/graph/DegreeOfKinship.java
@@ -0,0 +1,64 @@
+package baekjoon.ttone.graph;
+
+// #2644 graph 촌수계산 (dfs) 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class DegreeOfKinship {
+	
+	static List<Integer>[] relation;
+	static boolean[] checked;
+	static int res = Integer.MAX_VALUE;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		relation = new ArrayList[n+1];
+		checked = new boolean[n+1];
+		for(int i=1; i<n+1; i++) {
+			relation[i] = new ArrayList<>();
+		}
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		int l = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<l; i++) {
+			st = new StringTokenizer(br.readLine());
+			int p = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			relation[p].add(c);
+			relation[c].add(p);
+		}
+		
+		dfs(x,y, 0);
+		
+		System.out.println(res == Integer.MAX_VALUE ? -1 : res);
+	}
+	
+	static void dfs(int start, int end, int cnt) {
+	
+		if(start == end) {
+//			System.out.println("coin!"+ cnt);
+			res = Math.min(res, cnt);
+			return; 
+		}
+		
+		checked[start] = true;
+		for(int i=0; i<relation[start].size(); i++) { 
+//			System.out.println(relation[start].get(i));
+			int next = relation[start].get(i);
+			if(!checked[next]) {
+				dfs(next, end, cnt+1);
+			}
+		}
+		checked[start] = false;
+	}
+}
+
+
