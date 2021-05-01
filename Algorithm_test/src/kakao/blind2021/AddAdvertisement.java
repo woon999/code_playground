4f25a55b19bd0e3ac547ef510fad61cbfd13d337
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-01 20:32:20 +0900

    kakao 2021 #5 광고삽입 (구간합)

--- Algorithm_test/src/kakao/blind2021/AddAdvertisement.java
+++ Algorithm_test/src/kakao/blind2021/AddAdvertisement.java
@@ -1,9 +1,9 @@
 package kakao.blind2021;
 
-// blind #5 광고 삽입 (스케쥴링, 우선순위 큐) - 다시풀기 
-import java.util.*;
+// blind #5 광고 삽입  (최대 구간합) - 다시풀기 
 
-// 1. String data를 int로 다룰 수 있는지 생각하기 
+// 1. String data를 int로 다룰 수 있는지 생각하기
+// 2. 공간 크기 제한, 이중 포문 시간 복잡도 고려 
 
 public class AddAdvertisement {
 
@@ -18,113 +18,78 @@ public class AddAdvertisement {
 	}
 	
 	public static String solution(String play_time, String adv_time, String[] logs) {
-        String answer = "";
+		int play_len = timeToInt(play_time);
+		int adv_len = timeToInt(adv_time);
+		System.out.println(play_len);
+		System.out.println(adv_len);
+		
+		int[] ad = new int[360_000];
+		
+		for(String log : logs) {
+			String[] l = log.split("-");
+			int start = timeToInt(l[0]);
+			int end =  timeToInt(l[1]);
+//			System.out.println(timeToInt(l[0]) + " ~ " + timeToInt(l[1]));
+			for(int i=start; i<end; i++) {
+				ad[i]++;
+			}
+		}
+		
+		// 최대 시청 인원 구간 찾기 
+		// 최대 구간합 ( adv_time )
+		// 구간합을 이중 포문이 아닌 queue와 같이 구간 맨 앞의 값을 빼주고 새로운 값 추가하는 식으로 효율성증가   
+		int max_idx=0;
+		long max_sum=0;
+		long sum =0;
+		for(int i=0; i<adv_len; i++) {
+			sum += ad[i];
+		}
+		max_sum = sum;
+		
+		for(int i = adv_len; i<play_len; i++) {
+			sum += ad[i] - ad[i-adv_len];
+			if(sum > max_sum) {
+				max_sum = sum;
+				max_idx = i-adv_len+1;
+			}
+		}
+		
+		System.out.println(max_sum +" : " +timeToString(max_idx));
         
-//        String[] play_times = play_time.split(":");
-//        String[] adv_times = adv_time.split(":");
-        
-        int ad_time = Integer.parseInt(adv_time.replaceAll(":", ""));
-        
-        
-        Queue<UserData> data = new PriorityQueue<>();
-        for(int i=0; i<logs.length; i++) {
-        	String[] log = logs[i].split("-");
-//        	System.out.println(log[0]+"--"+log[1]);
-        	int a = Integer.parseInt(log[0].replaceAll(":", ""));
-        	int b = Integer.parseInt(log[1].replaceAll(":", ""));
-        	data.add(new UserData(i+1, a, b));
-        	
-        }
-        System.out.println();
-   
-        
-        UserData pos = data.poll();
-    	int start = pos.start;
-    	int end = pos.end;
-//    	System.out.println("시간 : " +start + " ~ " + end);
-    	
-    	int max = -1;
-    	int adStartTime = 0;
-    	int adCount=1; // 광고 시청 인원 
-    	int exposureTime =0; //광고 노출시간 
-        while(!data.isEmpty()) {
-        	UserData next = data.poll();
-        	int n_start = next.start;
-        	int n_end = next.end;
-        	
-//        	System.out.println("시간 : " +n_start + " ~ " + n_end);
-        	
-        	if(end >= n_start) {
-        		
-        		if(adCount ==1) {
-        			start =n_start;
-        			exposureTime += (end-start);
-//        			System.out.println("play : " +start + " ~ " + end +"=" + (end-start));
-        		}
-        		adCount ++;
-        		exposureTime += (end-n_start);
-//        		System.out.println("play : " +  n_start +" ~ " + end +"=" + (end-n_start));
-//        		System.out.println("ad start!" + n_start + " -> 시청인원:" + adCount +", 누적시간 : " + exposureTime);
-//        		if((end-n_start) >= ad_time){
-//        			
-//        			System.out.println((end -n_start)+" >="+ adv_time);
-//        			System.out.println("coin!");
-//        		}
-
-        	}
-        	else {
-        		
-        		start =n_start;
-        		end = n_end;
-//        		System.out.println("총 시청인원 : " + adCount);
-//        		System.out.println("광고 노출시간 :" + exposureTime );
-//        		System.out.println("reset! new :" + start+ "~"+end);
-        		adCount =1;
-        		exposureTime=0;
-//        		System.out.println();
-        	}
-        	
-        	if(exposureTime > max) {
-        		max = exposureTime;
-        		adStartTime = start;
-        	}
-        }
-        
-//       System.out.println("최고 시청인원 :" + max);
-//       System.out.println("광고 노출시간 :" + exposureTime );
-//       System.out.println("광고 시작시간 :" + adStartTime );
-       
-       answer += adStartTime;
-       while(answer.length() < 6) {
-    	   answer = "0" + answer;
-       }
-       
-       
-       String hour = answer.substring(0, 2);
-       String minutes = answer.substring(2, 4);
-       String seconds = answer.substring(4, 6);
-       
-        return hour+":"+minutes+":"+seconds;
+		return timeToString(max_idx);
     }
 	
-
-}
-
-class UserData implements Comparable<UserData>{
-	int userNum;
-	int start;
-	int end;
+	static int timeToInt(String time) {
+		String[] times = time.split(":");
+		int toSec = 3600;
+		int totalTime = 0;
+		for(String t : times) {
+			int num = Integer.parseInt(t);
+			totalTime += num*toSec;
+			toSec /= 60;
+		}
+		return totalTime;
+	}
 	
-	public UserData(int userNum, int start, int end) {
-		this.userNum = userNum;
-		this.start = start;
-		this.end = end;
+	static String timeToString(int time) {
+		String t = "";
+		int hour = time/3600;
+		time %= 3600;
+		if(hour <10) t+= "0"+ hour +":";
+		else t += hour+":";
+		
+		int minute = time/60;
+		time %= 60;
+		if(minute <10) t+= "0"+ minute+":";
+		else t += minute+":";
+		
+		int second = time;
+		if(second <10) t+= "0"+ second;
+		else t += second;
+		
+		return t;
 	}
+	
 
-	// start기준으로 오름차순
-	@Override
-	public int compareTo(UserData o) {
-		// TODO Auto-generated method stub
-		return this.start- o.start;
-	}
 }
+
