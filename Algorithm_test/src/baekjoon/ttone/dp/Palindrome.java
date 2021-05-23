e51804e43eb17701f4c6cc3019e205ac87034cbb
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-23 16:46:05 +0900

    bj dp,문자열 (10942. 펠린드롬)

--- Algorithm_test/src/baekjoon/ttone/dp/Palindrome.java
+++ Algorithm_test/src/baekjoon/ttone/dp/Palindrome.java
@@ -1,6 +1,6 @@
 package baekjoon.ttone.dp;
 
-// #10942 dp,문자열 펠린드롬 - 중간저장 
+// #10942 dp,문자열 펠린드롬  
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
@@ -8,17 +8,23 @@ import java.util.StringTokenizer;
 public class Palindrome {
 
 	static int n;
+	static int[] num;
+	static int[][] dp;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		n = Integer.parseInt(br.readLine());
-		
-		int[] num = new int[n];
+		num = new int[n+1];
+		dp = new int[n+1][n+1];
+		for(int i=0; i< n+1; i++) {
+			Arrays.fill(dp[i], -1);
+		}
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
+		for(int i=1; i<n+1; i++) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
+		
 		
 		int t = Integer.parseInt(br.readLine());
 		for(int i=0; i<t; i++) {
@@ -26,7 +32,7 @@ public class Palindrome {
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			
-			boolean flag = checkPalin(Arrays.copyOfRange(num, start-1, end));
+			boolean flag = checkPalin(start, end) == 1? true : false;
 			
 			if(flag) {
 				sb.append("1\n");
@@ -37,42 +43,18 @@ public class Palindrome {
 		
 		System.out.println(sb.toString());
 	}
-
-	static boolean checkPalin(int[] pattern) {
-		int len = pattern.length;
-		
-		int[] left;
-		int[] right;	
-		if(len %2 ==1) {
-			left = Arrays.copyOfRange(pattern, 0, (len-1)/2);
-			right = Arrays.copyOfRange(pattern, (len-1)/2+1, len);
-		}else {
-			left = Arrays.copyOfRange(pattern, 0, len/2);
-			right = Arrays.copyOfRange(pattern, len/2, len);
-			
-		}
-		
-//		for(int l : left) {
-//			System.out.print(l+" ");
-//		}
-//		System.out.println();
-//		for(int r : right) {
-//			System.out.print(r+" ");
-//		}
-//		System.out.println();
-		
-		int end = right.length-1;
-		for(int i=0; i< right.length; i++) {
-			if(left[i] != right[end-i]) {
-				return false;
-			}
-			
-		}
-		
-		return true;
-		
-		
-	}
 	
 
+	static int checkPalin(int s, int e) {
+		if( s >= e) return 1;
+		
+		if(dp[s][e] != -1) return dp[s][e];
+		
+		if(num[s]==num[e]) {
+			return dp[s][e] = checkPalin(s+1, e-1);
+		}
+		return 0;
+	
+	}
 }
+
