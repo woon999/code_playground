825bbf4dba3dee836a64add6d36931fb10788821
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-25 17:22:07 +0900

    update: 단방향 -> 양방향탐색  (bj dp,traversal (2213. 트리의 독립집합))

--- Algorithm_test/src/baekjoon/ttone/dp/TreeIndependentSet.java
+++ Algorithm_test/src/baekjoon/ttone/dp/TreeIndependentSet.java
@@ -9,6 +9,7 @@ public class TreeIndependentSet {
 	static List<Integer>[] node_list;
 	static int[] weight;
 	static int[][] memo;
+	static boolean[] check;
 	static List<Integer> res;	
 
 	public static void main(String[] args) throws IOException{
@@ -19,6 +20,7 @@ public class TreeIndependentSet {
 		weight = new int[n+1];
 		node_list = new ArrayList[n+1];
 		memo = new int[n+1][2];
+		check = new boolean[n+1];
 		res = new ArrayList<>();
 		for(int i=0; i<n+1; i++) {
 			node_list[i] = new ArrayList<>();
@@ -35,11 +37,8 @@ public class TreeIndependentSet {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 		
-			if(a>b) {
-				node_list[b].add(a);
-			}else {
-				node_list[a].add(b);
-			}
+			node_list[b].add(a);
+			node_list[a].add(b);
 		}
 //		for(int i=1; i<n+1; i++) {
 //        	if(node_list[i].size() >0) {
@@ -79,42 +78,52 @@ public class TreeIndependentSet {
 		
 		if(child_num ==0) return;
 		
+		check[pos] = true;
+		
 		for(int child : node_list[pos]) {
-			traversal(child);
-			
-			// 자식 x  > 자식 o
-			if(memo[child][0] > memo[child][1]) {
-				memo[pos][0] += memo[child][0]; // 부모 x 자식 x 
+			if(!check[child]) {
+				traversal(child);
 				
-			}else { //  자식 x  < 자식 o
-				memo[pos][0] += memo[child][1]; // 부모 x 자식 o
+				// 자식 x  > 자식 o
+				if(memo[child][0] > memo[child][1]) {
+					memo[pos][0] += memo[child][0]; // 부모 x 자식 x 
+					
+				}else { //  자식 x  < 자식 o
+					memo[pos][0] += memo[child][1]; // 부모 x 자식 o
+				}
+				
+				memo[pos][1] += memo[child][0]; // (공통) 부모 o 자식 x
 			}
-			
-			memo[pos][1] += memo[child][0]; // (공통) 부모 o 자식 x
 		}
+		
+		check[pos] = false;
 		
 	}
 	
 	static void trace(int pos, int attend) {
+		check[pos] = true;
 		if(attend==1) {
 			res.add(pos);
 			for(int i=0; i<node_list[pos].size(); i++) {
 				int next = node_list[pos].get(i);
-				
-				trace(next, 0);
+				if(!check[next]) {
+					trace(next, 0);
+				}
 			}
 		}
 		else {
 			for(int i=0; i<node_list[pos].size(); i++) {
 				int next = node_list[pos].get(i);
-				
-				if(memo[next][1] > memo[next][0]) {
-					trace(next, 1);
-				}else {
-					trace(next, 0);
+				if(!check[next]) {
+					if(memo[next][1] > memo[next][0]) {
+						trace(next, 1);
+					}else {
+						trace(next, 0);
+					}
 				}
 			}
 		}
+		check[pos] = false;
 		
 	}
 }
