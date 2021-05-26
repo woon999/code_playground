7a0b1b84dd54b70085d7d8fcfdb3cdff4b22f377
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-26 19:10:49 +0900

    update : dp풀이추가 - bj dp (2839. 설탕배달)

--- Algorithm_test/src/baekjoon/ttone/dp/SugarDelivery.java
+++ Algorithm_test/src/baekjoon/ttone/dp/SugarDelivery.java
@@ -2,37 +2,59 @@ package baekjoon.ttone.dp;
 
 // #2839 dp 설탕배달 
 import java.io.*;
+import java.util.Arrays;
 
 public class SugarDelivery {
 	
-	static int count =0;
-
+	static int INF = Integer.MAX_VALUE;
+	static int res;
+	static int[] dp;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int n = Integer.parseInt(br.readLine());
-		solve(n);
+		dp = new int[n+1];
+		Arrays.fill(dp,  -1);
+		res = INF;
+//		solve(n);
+		
+		System.out.println(solve(n,0) == INF ? -1 : res);
 		
 	}
-	
-	static void solve(int n) {
+	static int solve(int pos, int count) {
 		
-		while(true) {
-			if(n%5 ==0) {
-				count += n/5;
-				System.out.println(count);
-				return;
-			}else {
-				n -= 3;
-				count++;
-			}
-			
-			if(n<0) {
-				System.out.println(-1);
-				return;
-			}
-	
+		if(pos<0) return 0;
+		if(dp[pos] != -1) return dp[pos];
+		
+		if(pos ==0) {
+			res = Math.min(res, count);
+			return res;
 		}
 		
+		dp[pos] = Math.min(solve(pos-5, count+1), solve(pos-3, count+1));
+		
+		return res;
+		
 	}
+	
+//	static void solve(int n) {
+//		
+//		while(true) {
+//			if(n%5 ==0) {
+//				count += n/5;
+//				System.out.println(count);
+//				return;
+//			}else {
+//				n -= 3;
+//				count++;
+//			}
+//			
+//			if(n<0) {
+//				System.out.println(-1);
+//				return;
+//			}
+//	
+//		}
+//		
+//	}
 }
