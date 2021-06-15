fbc85a0d7d51247f1f0b764e14ff0fc04727b9d7
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-15 14:23:37 +0900

    bj dp,bitmasking (2089. 외판원순회)

---
+++ Algorithm_test/src/baekjoon/ttone/dp/TSP.java
@@ -0,0 +1,64 @@
+package baekjoon.ttone.dp;
+
+// #2089 dp, bitmasking 외판원순회 
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class TSP {
+
+	static int n, statusFullBit, INF = 987654321;
+	static int[][] w;
+	static int[][] dp;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		n = Integer.parseInt(br.readLine());
+		statusFullBit = (1<<n) -1;
+		w = new int[n][n];
+		dp = new int[n][statusFullBit];
+		for(int i=0; i<n; i++) {
+			Arrays.fill(dp[i], INF);
+		}
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<n; j++) {
+				w[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		System.out.println(tsp(0,1));
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<statusFullBit; j++) {
+				int num = dp[i][j];
+				if(num == INF) {
+					System.out.print(0+" ");
+				}else {
+					System.out.print(dp[i][j] +" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println(1 | (1<<3));
+	}
+	
+	static int tsp(int x, int check) {
+		if(check == statusFullBit) {
+			if(w[x][0] == 0) return INF;
+			else return w[x][0];
+		}
+		if(dp[x][check] != INF) return dp[x][check];
+		
+		for(int i=0; i<n; i++) {
+			int next = check | (1<<i);
+			// 경로가 없거나 i 도시를 이미 방문했을 경우 continue 
+			if(w[x][i] ==0 || (check & (1<<i)) != 0) continue;
+			
+			dp[x][check] = Math.min(dp[x][check], tsp(i, next) + w[x][i]);
+		}
+		
+		return dp[x][check];
+	}
+}
