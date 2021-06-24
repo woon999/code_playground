3122f6811eb6f5f0e11383fc38cad14ffda94e8d
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-24 14:00:00 +0900

    update : bfs 로직 추가 - bj graph (2468.안전영역)

--- Algorithm_test/src/baekjoon/ttone/graph/SafeZone.java
+++ Algorithm_test/src/baekjoon/ttone/graph/SafeZone.java
@@ -4,13 +4,15 @@ package baekjoon.ttone.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class SafeZone {
 
 	static int n;
 	static int[][] map;
-	static boolean[][] checked;
+	static boolean[][] check;
 	static int[] dx = {1, 0, -1, 0};
 	static int[] dy = {0,- 1, 0, 1};
 	public static void main(String[] args) throws IOException{
@@ -35,11 +37,11 @@ public class SafeZone {
 		
 		int max =0;
 		for(int height=0; height<maxHeight+1; height++) {
-			checked = new boolean[n][n];
+			check = new boolean[n][n];
 			int cnt=0;
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<n; j++) {
-					if(!checked[i][j] && map[i][j] > height){
+					if(!check[i][j] && map[i][j] > height){
 						cnt+=dfs(i,j,height);
 					}
 					
@@ -52,16 +54,44 @@ public class SafeZone {
 	
 	static int dfs(int x, int y, int height) {
 		
-		checked[x][y] = true;
+		check[x][y] = true;
 		for(int i=0; i<4; i++) {
 			int nx = x +dx[i];
 			int ny = y +dy[i];
 			
 			if(nx<0 || ny<0 || nx>n-1 || ny >n-1) continue;
-			if(checked[nx][ny]) continue;
+			if(check[nx][ny]) continue;
 			
 			if(map[nx][ny]> height) {
 				dfs(nx,ny, height);
+			}
+		}
+		
+		return 1;
+	}
+	
+	// bfs (느림) 
+	static int bfs(int x, int y, int height) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[]{x,y});
+		check[x][y] = true;
+		
+		while(!q.isEmpty()) {
+			int[] pos = q.poll();
+			int px = pos[0];
+			int py = pos[1];
+			
+			for(int i=0; i<4; i++) {
+				int nx = px +dx[i];
+				int ny = py +dy[i];
+				
+				if(nx<0 || ny<0 || nx>n-1 || ny >n-1) continue;
+				if(check[nx][ny]) continue;
+				
+				if(map[nx][ny]> height) {
+					check[nx][ny] = true;
+					q.add(new int[] {nx,ny});
+				}
 			}
 		}
 		
