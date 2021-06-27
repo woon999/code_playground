726862dafcd84a5452f94c5f99c90d61c28ef44b
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-27 16:39:32 +0900

    bj dataStructure (4195.친구네트워크) - union-find

--- Algorithm_test/src/baekjoon/ttone/dataStructure/FriendNetwork.java
+++ Algorithm_test/src/baekjoon/ttone/dataStructure/FriendNetwork.java
@@ -1,6 +1,6 @@
 package baekjoon.ttone.dataStructure;
 
-// #4195 dataStructure 친구 네트워크 - 중간저장 
+// #4195 dataStructure 친구 네트워크 - union-find 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,8 +8,8 @@ import java.util.*;
 
 public class FriendNetwork {
 
-	static int cnt;
 	static int[] parents, rank;
+	static int size = 100_001;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
@@ -20,15 +20,15 @@ public class FriendNetwork {
 		for(int test=0; test<t; test++) {
 			int f = Integer.parseInt(br.readLine());
 			
-			parents = new int[2*f];
-			rank = new int[2*f];
-			for(int i=1; i<2*f; i++) {
+			parents = new int[2*size];
+			rank = new int[2*size];
+			for(int i=1; i<2*size; i++) {
 				parents[i] = i;
+				rank[i] = 1;
+				
 			}
 			
 			int idx=1;
-			
-			cnt =0;
 			for(int i=0; i<f; i++) {
 				String line = br.readLine();
 				st = new StringTokenizer(line);
@@ -42,16 +42,9 @@ public class FriendNetwork {
 				String[] network = line.split(" ");
 				int to = map.get(network[0]);
 				int from = map.get(network[1]);
-//				System.out.println(to +" ," + from);
 				
-				union(to,from);
-				
-				for(int p=1; p<2*f; p++) {
-					System.out.print(rank[p]+" ");
-				}
-				System.out.println();
-				
-				sb.append(cnt+"\n");
+				int num = union(to,from);
+				sb.append(num+"\n");
 			}
 		}
 		
@@ -66,27 +59,21 @@ public class FriendNetwork {
 		return rx;
 	}
 	
-	static void union(int x, int y) {
+	static int union(int x, int y) {
 		x = find(x);
 		y = find(y);
 		
-		System.out.println(x+","+y);
-		if(rank[x] < rank[y]) { // y가 부모
-			parents[x] = y;
-			rank[y]++;
-		}else {
-			parents[y] = x;
-			
-			if(rank[x]== rank[y]) {
-				rank[x]++;
+		if(x != y) { 
+			if(x>y) { // y가 부모
+				parents[x] = y;
+				rank[y]+= rank[x];
+				return rank[y];
 				
-				if(rank[y]!=0 && x!=y) {
-					rank[x]++;
-					rank[y]--;
-				}
+			}else{  //x가 부모
+				parents[y] = x;
+				rank[x]+=rank[y];
 			}
-		}
-		
+		} 
+		return rank[x];
 	}
-	
 }
