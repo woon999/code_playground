6b5051fe069fc83e9efdc8a3d1f59736953ffad2
jongwon <54282927+loosie@users.noreply.github.com>
2021-06-29 16:39:41 +0900

    update: bfs로직 추가 (bj graph (13460. 구슬 탈출2)

--- Algorithm_test/src/baekjoon/ttone/graph/EscapeBiz2.java
+++ Algorithm_test/src/baekjoon/ttone/graph/EscapeBiz2.java
@@ -1,8 +1,10 @@
 package baekjoon.ttone.graph;
 
-// #13460 graph 구슬탈출2  (dfs, backtracking) 
+// #13460 graph 구슬탈출2  (dfs, backtracking) -update : bfs  
 // 그런데 미로찾기, 최단 거리 그래프 탐색은 bfs로 하는게 더 효율적 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class EscapeBiz2 {
@@ -24,7 +26,6 @@ public class EscapeBiz2 {
 		
 		int rx =0, ry =0;
 		int bx =0, by =0;
-		
 		for(int i=0; i<n; i++) {
 			String[] line = br.readLine().split("");
 			for(int j=0; j<m; j++) {
@@ -39,8 +40,74 @@ public class EscapeBiz2 {
 			}
 		}
 		
-		dfs(rx,ry,bx,by,0);
+		bfs(rx,ry,bx,by,0);
 		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
+	}
+	
+	static void bfs(int rx, int ry, int bx, int by, int cnt) {
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {rx,ry,bx,by, cnt});
+		checked[rx][ry][bx][by] =true;
+		
+		while(!q.isEmpty()) {
+			int[] pos = q.poll();
+			int pCnt = pos[4];
+			
+			
+			if(pCnt>=10) {
+				return;
+			}
+			
+			for(int i=0; i<4; i++){
+				int nRx = pos[0];
+				int nRy = pos[1];
+				int nBx = pos[2];
+				int nBy = pos[3];
+				
+				// 빨간 구슬 이동 
+				while(map[nRx+dx[i]][nRy+dy[i]] != 0) {
+					nRx += dx[i];
+					nRy += dy[i];
+					if(map[nRx][nRy] == 44) break;
+				}
+				
+				// 파란 구슬 이동
+				while(map[nBx+dx[i]][nBy+dy[i]] != 0) {
+					nBx += dx[i];
+					nBy += dy[i];
+					if(map[nBx][nBy] == 44) break;
+				}
+				
+				// 파란 구슬이 구멍에 들어갔을 때
+				if(map[nBx][nBy] == 44) continue;
+				
+				if(map[nRx][nRy] == 44) {
+					min = pCnt+1;
+					return;
+				}
+				
+				// 빨간 파랑 서로 만났을 때 
+				if(nRx == nBx && nRy == nBy && map[nRx][nRy] != 44) { 
+					int red_move = Math.abs(nRx-pos[0]) + Math.abs(nRy-pos[1]);
+					int blue_move = Math.abs(nBx-pos[2]) + Math.abs(nBy-pos[3]);
+					
+					// 파란 공이 더 빨리 도착한 경우  
+					if(red_move > blue_move) {
+						nRx -= dx[i];
+						nRy -= dy[i];
+					}else { // 빨간 공이 더 빨리 도착한 경우  
+						nBx -= dx[i];
+						nBy -= dy[i];
+					}
+				}
+				
+				if(!checked[nRx][nRy][nBx][nBy]) {
+					checked[nRx][nRy][nBx][nBy] = true;
+					q.add(new int[] {nRx, nRy, nBx, nBy, pCnt+1});
+				}
+			}
+		}
+		
 	}
 	
 	static void dfs(int rx, int ry, int bx, int by, int cnt) {
