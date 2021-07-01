56ccd864b7edcd2c7a859bcbec136bd527248eef
jongwon <54282927+loosie@users.noreply.github.com>
2021-07-01 20:54:23 +0900

    update : 최소힙,최대힙,map 풀이 추가 (7662. 이중 우선순위 큐)

--- Algorithm_test/src/baekjoon/ttone/dataStructure/DualPriorityQueue.java
+++ Algorithm_test/src/baekjoon/ttone/dataStructure/DualPriorityQueue.java
@@ -1,11 +1,17 @@
 package baekjoon.ttone.dataStructure;
 
-// #7662 dataStructure 이중 우선순위 큐 (TreeMap) 
+// #7662 dataStructure 이중 우선순위 큐 (최대힙, 최소힙, Map)  
 import java.io.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class DualPriorityQueue {
+	
+	static Map<Integer, Integer> map;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,39 +19,56 @@ public class DualPriorityQueue {
 		int t = Integer.parseInt(br.readLine());
 		for(int test=0; test<t; test++) {
 			int input = Integer.parseInt(br.readLine());
-
-			TreeMap<Integer, Integer> map = new TreeMap<>();
+			
+			Queue<Integer> min = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순 
+			Queue<Integer> max = new PriorityQueue<>();
+			map = new HashMap<>();
 			for(int i=0; i<input; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				String op = st.nextToken();
 				
 				if(op.equals("I")) {
 					int num = Integer.parseInt(st.nextToken());
+					min.add(num);
+					max.add(num);
 					map.put(num, map.getOrDefault(num, 0)+1);
 				}else {
-					if(map.size()==0) continue;
 					int type = Integer.parseInt(st.nextToken());
-					int num;
+					
+					if(map.size()==0) continue;
 					if(type == 1) { //최댓값 삭제 
-						num = map.lastKey();
+						delete(min);
 					}else { // 최솟값 삭제
-						num = map.firstKey();
-					}
-					if(map.put(num, map.get(num)-1)==1) {
-						map.remove(num);
+						delete(max);
 					}
 				}
 			}
 			
-			
-			
 			if (map.size()==0) {
 	            sb.append("EMPTY\n");
 	        } else {
-	        	sb.append(map.lastKey()+" " + map.firstKey()+"\n");
+	        	int res = delete(min);
+	        	sb.append(res+" "); // 최댓값 
+	        	if(map.size()>0) res = delete(max);
+	        	sb.append(res+"\n"); // 최솟값
 	        }
-
 		}
 		System.out.println(sb.toString());
+	}
+	
+	static int delete(Queue<Integer> q) {
+		int res = 0;
+		while(true) {
+			res = q.poll();
+			
+			int cnt = map.getOrDefault(res, 0);
+			if(cnt ==0) continue;
+			
+			if(cnt ==1) map.remove(res);
+			else map.put(res, cnt-1);
+			break;
+		}
+		
+		return res;
 	}
 }
---
+++ Algorithm_test/src/baekjoon/ttone/dataStructure/DualPriorityQueue2.java
@@ -0,0 +1,51 @@
+package baekjoon.ttone.dataStructure;
+
+// #7662 dataStructure 이중 우선순위 큐 (TreeMap) 
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class DualPriorityQueue2 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for(int test=0; test<t; test++) {
+			int input = Integer.parseInt(br.readLine());
+
+			TreeMap<Integer, Integer> map = new TreeMap<>();
+			for(int i=0; i<input; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String op = st.nextToken();
+				
+				if(op.equals("I")) {
+					int num = Integer.parseInt(st.nextToken());
+					map.put(num, map.getOrDefault(num, 0)+1);
+				}else {
+					if(map.size()==0) continue;
+					int type = Integer.parseInt(st.nextToken());
+					int num;
+					if(type == 1) { //최댓값 삭제 
+						num = map.lastKey();
+					}else { // 최솟값 삭제
+						num = map.firstKey();
+					}
+					if(map.put(num, map.get(num)-1)==1) {
+						map.remove(num);
+					}
+				}
+			}
+			
+			
+			
+			if (map.size()==0) {
+	            sb.append("EMPTY\n");
+	        } else {
+	        	sb.append(map.lastKey()+" " + map.firstKey()+"\n");
+	        }
+
+		}
+		System.out.println(sb.toString());
+	}
+}
