eb242d6616108cdcf6c99c26a277d704706d3872
jongwon <54282927+loosie@users.noreply.github.com>
2021-07-25 16:41:24 +0900

    prog k21itrn (5. 시험장 나누기) - 트리 dp, 파라메트릭 서치

--- Algorithm_test/src/kakao/intern2021/DivisionExamRoom.java
+++ Algorithm_test/src/kakao/intern2021/DivisionExamRoom.java
@@ -1,6 +1,6 @@
 package kakao.intern2021;
 
-// #5 kakao2021intern 시험장 나누기 - 중간저장 
+// #5 kakao2021intern 시험장 나누기 - 트리 dp, 파라메트릭 서치 
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +9,7 @@ public class DivisionExamRoom {
 	static class Node{
 		int data;
 		int left, right;
-		public Node() {
-			/* no-op */
-		}
+		
 		
 		public Node(int data, int left, int right) {
 			this.data = data;
@@ -19,26 +17,30 @@ public class DivisionExamRoom {
 			this.right = right;
 		}
 	}
-	static int sectionSize, root, total=0, depth=-1;
+	static int INF = 789654321, maxSection =10001;
 	static List<Node>[] list;
 	static int[][] cost;
 	public static void main(String[] args) {
-		int k = 2;
-		int[] num = {6,9,7,5};
-		int[][] links = {{-1, -1}, {-1, -1}, {-1, 0}, {2, 1}};
+//		int k = 1;
+//		int[] num = {6,9,7,5};
+//		int[][] links = {{-1, -1}, {-1, -1}, {-1, 0}, {2, 1}};
 		
-		solution(k, num, links);
+		int k = 3;
+		int[] num = {12, 30, 1, 8, 8, 6, 20, 7, 5, 10, 4, 1};
+		int[][] links = {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, 
+						{8, 5}, {2, 10}, {3, 0}, {6, 1},
+						{11, -1}, {7, 4}, {-1, -1}, {-1, -1}};
+		
+		System.out.println(solution(k, num, links));
 	}
 	
 	public static int solution(int k, int[] num, int[][] links) {
-        int answer = 0;
         int size = num.length;
-        sectionSize = k;
         list = new ArrayList[size];
-        cost = new int[size][k+1];
         for(int i=0; i<size; i++) {
         	list[i] = new ArrayList<>();
         }
+        int sum = 0;
         boolean[] check = new boolean[size];
         for(int i=0; i<size; i++) {
         	int left = links[i][0];
@@ -46,111 +48,140 @@ public class DivisionExamRoom {
         	if(left != -1) check[left] = true;
         	if(right != -1) check[right] = true;
         	list[i].add(new Node(num[i], left, right));
-        	total += num[i];
-        	
-        }
+        	sum += num[i];
+        }	
         
-        root = -1;
+        int root = -1;
         for(int i=0; i<size; i++) {
         	if(!check[i]) root = i;
-        	Node pos = list[i].get(0);
-        	System.out.println(i+"번 노드 : " + (pos.left==-1?"null":pos.left) + " -  [" + pos.data + "] - " + (pos.right==-1?"null":pos.right) );
         }
+//        System.out.println("root : " + root);
         
-        System.out.println();
-        
-        traversal(root,0);
-        
-        System.out.println();
-        for(int a=0; a<size; a++) {
-        	System.out.print(a + "번 노드 : ");
-	        for(int i=1; i<k+1; i++	) {
-	        	System.out.print(cost[a][i]+ " " );
+        int left = sum/k;
+        int right = sum;
+        if(left == right) return right;
+        else {
+	        while(left+1 < right) {
+//	        	System.out.println();
+//	        	System.out.println(left+"," + right);
+	        	int mid = (left+right)/2;
+	        	cost = new int[size][2];
+	        	traversal(root, mid);
+//	        	System.out.println("result : " +cost[root][0] +" <= "+ k);
+	        	if(cost[root][0] <= k) {
+	        		right = mid;
+	        	}else {
+	        		left = mid;
+	        	}
 	        }
-	        System.out.println();
+	        return right;
         }
-        return answer;
+
     }
 	
-	static void traversal(int pos, int cnt) {
-		
+	static void traversal(int pos, int w) {
 		Node curNode = list[pos].get(0);
-//		System.out.println(curNode.left+" , " +pos + " ," + curNode.right);
+		int data = curNode.data;
+		int l = curNode.left;
+		int r = curNode.right;
 		
-		for(Node child : list[pos]) {
-			
-			if(child.left!=-1) traversal(child.left, cnt+1);
-			if(child.right!=-1) traversal(child.right, cnt+1);
-			
-			System.out.println("--------------------------");
-			System.out.println(pos + "번 노드 " + cnt);
-			System.out.println((child.left==-1?"null":child.left) + " -  [" + child.data + "] - " + (child.right==-1?"null":child.right) );
-			
-			
-			cost[pos][1] = child.data;
-			System.out.println(pos+" 데이터 저장 " + child.data);
-			int mSize = (int)Math.pow(2, depth+1 - cnt)-1;
-			
-			// leaf노드 
-			if(child.left==-1 && child.right == -1) {
-				depth = Math.max(depth, cnt);
+		if(l != -1) traversal(l,w);
+		if(r != -1) traversal(r,w);
+		
+//		System.out.println("===========================");
+//		System.out.println(pos + "번 노드 "  + w);
+//		System.out.println((curNode.left==-1?"null":curNode.left) + " -  [" + curNode.data + "] - " + (curNode.right==-1?"null":curNode.right) );
+//		System.out.println();
+		// leaf 노드 
+		if(l == -1 && r== -1) {
+			if(data <= w) {
+				cost[pos][0] = 1;
+				cost[pos][1] = data;
+			}else {
+				cost[pos][0] = maxSection;
+				cost[pos][1] = INF;
 			}
-
+		}
+		// full 노드 
+		else if(l!=-1&&r!=-1){
+//			System.out.println("====full=====");
+			// 1) pos + 왼쪽 트리 + 오른쪽 트리 <= L 
+			// section l+r-1
+			if(data + cost[l][1] + cost[r][1] <=w) {
+				cost[pos][0] = cost[l][0] + cost[r][0] -1;
+				cost[pos][1] = data + cost[l][1] + cost[r][1];
+//				System.out.println("#1 "  + cost[pos][0] + "," + cost[pos][1]);
+			}
+			// 2) pos + min(왼쪽, 오른쪽)  <= L
+			// section  l+r
+			else if(data + Math.min(cost[l][1],cost[r][1]) <= w) {
+				cost[pos][0] = cost[l][0] + cost[r][0];
+				cost[pos][1] = data + Math.min(cost[l][1], cost[r][1]);
+//				System.out.println("#2 "  + cost[pos][0] + "," + cost[pos][1]);
+			}
+			// 3) pos <= L
+			// section  l+r +1
+			else if(data <= w) {
+				cost[pos][0] = cost[l][0] + cost[r][0] +1;
+				cost[pos][1] = data;
+//				System.out.println("#3 "  + cost[pos][0] + "," + cost[pos][1]);
+				
+			}
 			else {
-//				System.out.println("최대분할개수 : " +mSize);
-				// 왼쪽 자식노드
-				if(child.left!=-1) {
-					for(int d=1; d<mSize+1; d++) {
-						if(d > sectionSize ) break;
-						if(d< sectionSize)  { //d < sectionSize
-							System.out.println((d+1) + " .#1 " + cost[pos][d] + " vs " +cost[child.left][d]);
-							cost[pos][d+1] = Math.max(cost[pos][d],cost[child.left][d]);
-							System.out.println(pos+","+(d+1) +" = " +cost[pos][d+1]);
-							System.out.println();
-						}
-						if( d < sectionSize) {
-							System.out.println(d+ ". #2 " + cost[pos][d] + " + " +cost[child.left][d]);
-							if(d==sectionSize) {
-								int comp1 = Math.max(total-cost[pos][d],cost[pos][d]);
-								int tmp = cost[pos][d] +cost[child.left][d];
-								int comp2 = Math.max(total-tmp, cost[pos][d] +cost[child.left][d]);
-								System.out.println(comp1 + "," +comp2);
-								if(comp1 > comp2) {
-									cost[pos][d] = cost[pos][d] +cost[child.left][d];
-								}
-							}else {
-								if(cost[pos][d] < cost[pos][d] +cost[child.left][d]) {
-									cost[pos][d] = cost[pos][d] +cost[child.left][d];
-//									System.out.println(pos+","+(d) +" = " +cost[pos][d]);
-									System.out.println();
-								}
-							}
-						}
-					}
+				cost[pos][0] = maxSection;
+				cost[pos][1] = INF;
+//				System.out.println("#4 "  + cost[pos][0] + "," + cost[pos][1]);
+			}
+		}else {
+			// 왼쪽 자식만 있는 경우
+			if(r == -1) {
+//				System.out.println("====left=====");
+				// 1) pos + 왼쪽 트리 <= L
+				// section l 
+				if(data + cost[l][1] <=w) {
+					cost[pos][0] = cost[l][0];
+					cost[pos][1] = data + cost[l][1];
+//					System.out.println("#1 "  + cost[pos][0] + "," + cost[pos][1]);
 				}
-				 
-				if(child.right!=-1) {
-					for(int d=1; d<mSize+1; d++) {
-						if(d > sectionSize ) break;// k==2
-						
-						if(d< sectionSize) { //d < sectionSize
-							System.out.println((d+1)+ " .#3 " + cost[pos][d]+ " vs " +cost[child.right][d]);
-							cost[pos][d+1] = Math.max(cost[pos][d],cost[child.right][d]);
-							System.out.println(pos+","+(d+1) +" = " +cost[pos][d+1]);
-							System.out.println();
-						}
-						if( d < sectionSize) {
-							System.out.println(d +" .#4 " + cost[pos][d] + " + " +cost[child.right][d]);
-							if(cost[pos][d] < cost[pos][d] +cost[child.right][d]) {
-								cost[pos][d] = cost[pos][d] +cost[child.right][d];
-//								System.out.println(pos+","+(d) +" = " +cost[pos][d]);
-								System.out.println();
-							}
-						}
-						
-					}
+				// 1) pos <= L
+				// section l +1  
+				else if(data <= w) {
+					cost[pos][0] = cost[l][0]+1;
+					cost[pos][1] = data;
+//					System.out.println("#2 "  + cost[pos][0] + "," + cost[pos][1]);
+				}
+				else {
+					cost[pos][0] = maxSection;
+					cost[pos][1] = INF;
+//					System.out.println("#3 "  + cost[pos][0] + "," + cost[pos][1]);
+				}
+			}
+			
+			// 오른쪽 자식만 있는 경우
+			if(l == -1) {
+//				System.out.println("====right=====");
+				// 1) pos + 오른쪽 트리 <= L
+				// section r 
+				if(data + cost[r][1] <=w) {
+					cost[pos][0] = cost[r][0];
+					cost[pos][1] = data + cost[r][1];
+//					System.out.println("#1 " + cost[pos][0] + "," + cost[pos][1]);
+				}
+				// 1) pos <= L
+				// section r +1  
+				else if(data <= w) {
+					cost[pos][0] = cost[r][0]+1;
+					cost[pos][1] = data;
+//					System.out.println("#2 "  + cost[pos][0] + "," + cost[pos][1]);
+				}
+				else {
+					cost[pos][0] = maxSection;
+					cost[pos][1] = INF;
+//					System.out.println("#3 "  + cost[pos][0] + "," + cost[pos][1]);
 				}
 			}
 		}
+		
+
 	}
 }
