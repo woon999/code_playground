2a063d899f1a05f4736f1b5d789b87598a8fb4f5
jongwon <54282927+loosie@users.noreply.github.com>
2021-07-29 14:56:33 +0900

    bj dataStructrue (5397. 키로거) -stack

--- Algorithm_test/src/baekjoon/ttone/dataStructure/KeyLogger.java
+++ Algorithm_test/src/baekjoon/ttone/dataStructure/KeyLogger.java
@@ -1,10 +1,10 @@
 package baekjoon.ttone.dataStructure;
 
+// #5397 dataStructrue 키로거 - stack  
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-
-// #5397 dataStructrue 키로거 - 중간 저장 (시간초과) 
+import java.util.Stack;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
@@ -16,27 +16,35 @@ public class KeyLogger {
 		
 		for(int i=0; i<n; i++) {
 			String line = br.readLine();
-			StringBuilder sb = new StringBuilder();
-			int cursor = 0;
+			Stack<Character> pwdStack = new Stack<>();
+			Stack<Character> deleteStack = new Stack<>();
 			for(int j=0; j<line.length(); j++) {
 				char pos = line.charAt(j);
 				if(pos == '<') {
-					cursor--;
-					if(cursor <0) cursor =0;
+					if(!pwdStack.isEmpty()) {
+						deleteStack.push(pwdStack.pop());
+					}
 				}else if(pos == '>') {
-					cursor++;
-					if(cursor>sb.length()) cursor=sb.length();
+					if(!deleteStack.isEmpty()) {
+						pwdStack.push(deleteStack.pop());
+					}
 				}else if(pos == '-') {
-					if(cursor>0) {
-						sb.deleteCharAt(cursor-1);
-						cursor--;
+					if(!pwdStack.isEmpty()) {
+						pwdStack.pop();
 					}
 				}else {
-					sb.insert(cursor, pos);
-					cursor++;
+					pwdStack.push(line.charAt(j));
 				}
 			}
-			bw.write(sb.toString()+"\n");
+			
+			while(!deleteStack.isEmpty()) {
+				pwdStack.push(deleteStack.pop());
+			}
+			
+			for(int k=0; k<pwdStack.size(); k++) {
+				bw.write(pwdStack.get(k));
+			}
+			bw.write("\n");
 		}
 		
 		bw.flush();
