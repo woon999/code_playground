dd6035ba86f83c4b837c91e0002612ac8a5bb475
jongwon <54282927+loosie@users.noreply.github.com>
2021-08-18 14:13:31 +0900

    bj dp (11726. 2xN 타일링)

---
+++ Algorithm_test/src/baekjoon/ttone/dp/TwoXNTiling.java
@@ -0,0 +1,24 @@
+package baekjoon.ttone.dp;
+
+// #11726 dp 2xN 타일링 
+import java.io.*;
+
+public class TwoXNTiling {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[n+1];
+		
+		dp[0] =1;
+		dp[1]= 1;  
+		for(int i=2; i<n+1; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
+}
