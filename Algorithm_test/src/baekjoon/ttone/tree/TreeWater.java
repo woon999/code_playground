b4ef6bcbb9de37250b983a4a533f6dc4dc255ca9
jongwon <54282927+loosie@users.noreply.github.com>
2021-10-24 14:05:23 +0900

    bj tree (17073. 나무 위의 빗물)

---
+++ Algorithm_test/src/baekjoon/ttone/tree/TreeWater.java
@@ -0,0 +1,47 @@
+package baekjoon.ttone.tree;
+
+// #17073 tree 나무 위의 빗물 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class TreeWater {
+
+	static int leafN =0;
+	static List<Integer>[] list;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		
+		list = new ArrayList[n+1];
+		for(int i=1; i<n+1; i++) {
+			list[i] = new ArrayList<>();
+		}
+		for(int i=0; i<n-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			list[a].add(b);
+			list[b].add(a);
+		}
+		
+		leafCounting(1, -1);
+		
+		System.out.println(String.format("%.10f", (double)w/leafN));
+	}
+	
+	static void leafCounting(int idx, int pa) {
+		if(pa!= -1 && list[idx].size()==1) leafN++;
+		for(int nxt : list[idx]) {
+			if(nxt != pa) {
+				leafCounting(nxt, idx);
+			}
+		}
+	}
+}
