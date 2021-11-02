c2e1ccf5228129a48721c7eb1e016b6603797558
jongwon <54282927+loosie@users.noreply.github.com>
2021-11-02 15:14:47 +0900

    update: bj implementation (1913. 달팽이)

--- Algorithm_test/src/baekjoon/ttone/implementation/Snail.java
+++ Algorithm_test/src/baekjoon/ttone/implementation/Snail.java
@@ -9,56 +9,93 @@ public class Snail {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int t = Integer.parseInt(br.readLine());
+		solve1(n,t);
+//		solve2(n,t);
 		
+	}
+	
+	static void solve2(int n, int t){
+		int[][] map = new int[n][n];
+		int value =1;
+		
+		int x = n/2, y=n/2;
+		
+		int limit =1;
+		while(true) {
+			for(int i=0; i<limit; i++) {
+				map[y--][x] = value++;
+			}
+			if(value-1 == n*n) break;
+			for(int i=0; i<limit; i++) {
+				map[y][x++] = value++;
+			}
+			
+			limit++;
+			for(int i=0; i<limit; i++) {
+				map[y++][x] = value++;
+			}
+			
+			for(int i=0; i<limit; i++) {
+				map[y][x--] = value++;
+			}
+			limit++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		int tx=0, ty=0;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(t==map[i][j]) {
+					ty=i+1;
+					tx=j+1;
+				}
+				sb.append(map[i][j] +" ");
+			}
+			sb.append("\n");
+		}
+		sb.append(ty+" "+tx);
+		System.out.println(sb.toString());
+	}
+	
+	static void solve1(int n, int t) {
 		int[][] map = new int[n][n];
 		int value = n*n;
-		int tx=0, ty=0;
-		int x=0,y=0;
-		int time=0;
-		int limit =n;
+		int x = 0,y = 0;
+		int time = 0;
+		int limit = n;
 		while(value>0) {
 			x=time;
 			for(int i=y; i<limit; i++) {
-				if(t== value) {
-					tx=x+1;
-					ty=i+1;
-				}
 				map[i][x] = value--;
 			}
 			
 			y=limit-1;
 			for(int i=x+1; i<limit; i++) {
-				if(t== value) {
-					tx=i+1;
-					ty=y+1;
-				}
 				map[y][i] = value--;
 			}
 			
 			x=limit-1;
 			for(int i=y-1; i>=time; i--) {
-				if(t== value) {
-					tx=x+1;
-					ty=i+1;
-				}
 				map[i][x] = value--;
 			}
 			
 			y=time;
 			for(int i=x-1; i>time; i--) {
-				if(t== value) {
-					tx=i+1;
-					ty=y+1;
-				}
 				map[y][i] = value--;
 			}
 			time++;
 			limit--;
 			y=time;
 		}
+		
 		StringBuilder sb = new StringBuilder();
+		int tx=0, ty=0;
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
+				if(t==map[i][j]) {
+					ty=i+1;
+					tx=j+1;
+				}
 				sb.append(map[i][j] +" ");
 			}
 			sb.append("\n");
