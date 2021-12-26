596a2c49ba25a55697fa826723fe1395c0872a64
jongwon <54282927+loosie@users.noreply.github.com>
2021-12-26 15:10:49 +0900

    bj twoPointer (1806. 부분합)

---
+++ algorithm/src/baekjoon/ttone/twoPointer/SubSum.java
@@ -0,0 +1,41 @@
+package baekjoon.ttone.twoPointer;
+
+// #1806 twoPointer 부분합 
+import java.io.*;
+import java.util.*;
+
+public class SubSum {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int start = 0;
+		int end = 0;
+		int len = Integer.MAX_VALUE;
+		int sum = 0;
+		while(start <= end && end <= n) {
+			if(sum < s) {
+				sum += arr[end++];
+			} else if(sum >= s) {
+				len = Math.min(len, end-start);
+				sum -= arr[start++];
+				
+			}  
+		}
+		
+		System.out.println(len==Integer.MAX_VALUE ? 0 : len);
+	}
+}
+
+
+
+
