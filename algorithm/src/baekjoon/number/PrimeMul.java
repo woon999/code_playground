3c02ae8bd0f0a004c5f439cf63f21b3423485c8c
jongwon <54282927+loosie@users.noreply.github.com>
2022-02-07 12:47:34 +0900

    bj number (2014. 소수의 곱)

---
+++ algorithm/src/baekjoon/number/PrimeMul.java
@@ -0,0 +1,37 @@
+package baekjoon.number;
+
+// #2014 number 소수의 곱 
+import java.io.*;
+import java.util.*;
+
+public class PrimeMul {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int k = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		
+		long[] prime = new long[k];
+		Queue<Long> q = new PriorityQueue<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<k; i++) {
+			prime[i] = Integer.parseInt(st.nextToken());
+			q.add(prime[i]);
+		}
+		
+		for(int i=0; i<n-1; i++) {
+			long num = q.poll();
+			for(int j=0; j<k; j++) {
+				long mul = num * prime[j];
+				q.add(mul);
+				
+				if(num%prime[j] ==0)break;
+			}
+		}
+		System.out.println(q.poll());
+		
+		
+	}
+}
