475a7f62b6a0d77c42e5d02a732093ec23e4763b
jongwon <54282927+loosie@users.noreply.github.com>
2022-03-30 19:02:32 +0900

    prog update

--- algorithm/src/kakao/bline2022/GetReport.java
+++ algorithm/src/kakao/bline2022/GetReport.java
@@ -3,7 +3,6 @@ package kakao.bline2022;
 // #1 kakao2022blind 신고 결과 받기 
 
 import java.util.*;
-
 public class GetReport {
 	
 	public static void main(String[] args) {
@@ -20,54 +19,41 @@ public class GetReport {
 	}
 
 	public static int[] solution(String[] id_list, String[] report, int k) {
-		Map<String, Integer> index = new HashMap<>();
-	    Map<String, String> userList = new HashMap<>();
-	    Map<String, Integer> reportMap = new HashMap<>();
+		Map<String, Set<String>> reportMap = new HashMap<>();
+		
+		for(String rep : report) {
+			String[] r = rep.split(" ");
+			
+			if(!reportMap.containsKey(r[1])) {
+				Set<String> set = new HashSet<>();
+				set.add(r[0]);
+				reportMap.put(r[1], set);
+			}else {
+				reportMap.get(r[1]).add(r[0]);	
+			}
+			
+		}
+		
+		Map<String, Integer> idMap = new LinkedHashMap<>();
+		for(String id : id_list) {
+			idMap.put(id, 0);
+		}
+		
+		for(String key : reportMap.keySet()) {
+			Set<String> list = reportMap.get(key);
+			if(list.size() >= k) {
+				for(String repId : list) {
+					idMap.put(repId, idMap.getOrDefault(repId, 0)+1);
+				}
+			}
+		}
+		
+		int[] answer = new int[id_list.length];
+		int idx = 0;
+		for(String key : idMap.keySet()) {
+			answer[idx++] = idMap.get(key);
+		}
 	    
-	    for(int i=0; i<id_list.length; i++) {
-	    	userList.put(id_list[i], "");
-	    	reportMap.put(id_list[i], 0);
-	    	index.put(id_list[i], i);
-	    }
-	    
-	    
-	    Set<String> reportSet = new HashSet<>();
-	    for(String r : report) {
-	    	reportSet.add(r);
-	    }
-	    
-	    for(String r : reportSet) {
-	    	String[] reportData = r.split(" ");
-	    	userList.put(reportData[0], userList.get(reportData[0])+reportData[1]+" ");
-	    	reportMap.put(reportData[1], reportMap.get(reportData[1])+1);
-	    }
-	    
-	    
-	    List<String> reportList = new ArrayList<>(reportMap.keySet());
-	    Collections.sort(reportList, (a,b) -> reportMap.get(b) - reportMap.get(a));
-	    
-	    int idx=0;
-	    for(int i=0; i<reportList.size(); i++) {
-	    	String key = reportList.get(i);
-	    	if(reportMap.get(key) <k) {
-	    		idx = i;
-	    		break;
-	    	}
-	    }
-	    
-	    reportList = reportList.subList(0, idx);
-	    int[] answer = new int[id_list.length];
-	    // 비교 
-	    for(String key : userList.keySet()) {
-	    	int cnt = 0;
-	    	String[] list = userList.get(key).split(" ");
-	    	for(int i=0; i<list.length; i++) {
-	    		if(reportList.contains(list[i])) {
-	    			cnt++;
-	    		}
-	    	}
-	    	answer[index.get(key)] = cnt;	
-	    }
 	    return answer;
 	}
 }
