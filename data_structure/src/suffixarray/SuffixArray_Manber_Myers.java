484542c03480230474ed030dc496dd2a9c775ef6
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-09 00:12:31 +0900

    feat: suffix array 배열 크기 수정

--- data_structure/src/suffixarray/SuffixArray_Manber_Myers.java
+++ data_structure/src/suffixarray/SuffixArray_Manber_Myers.java
@@ -12,13 +12,12 @@ import java.util.List;
  * 배열 lgn번 정렬
  * 전체 시간복잡도 O(n*(lgn)^2)
  */
-
 public class SuffixArray_Manber_Myers {
 	public static void main(String[] args) {
 		String text = "banana";
 		List<Integer> suffix = getSuffixArray(text);
 
-		for(int i : suffix){
+		for (int i : suffix) {
 			System.out.println(i + " - " + text.substring(i));
 		}
 	}
@@ -30,48 +29,50 @@ public class SuffixArray_Manber_Myers {
 		// 접미사 배열이 될 반환 값 (이 동적 배열을 log(n)번 정렬)
 		List<Integer> perm = new ArrayList<>();
 		// group
-		int[] group = new int[2*n];
-		Arrays.fill(group, -1);
+		int[] group = new int[n + 1];
 		for (int i = 0; i < n; i++) {
 			perm.add(i);
 			// text가 영어 소문자로만 이루어졌다고 가정
 			group[i] = text.charAt(i) - 'a';
 		}
+		group[n] = -1;
 
-		CompUsing2T compUsing2T = new CompUsing2T(t , group);
+		CompUsing2T compUsing2T = new CompUsing2T(n, t, group);
 		while (t < n) {
 			// group[]은 첫 t글자를 기준으로 계산해뒀다. 첫 2t글자를 기준으로 perm을 다시 정렬한다.
 			Collections.sort(perm, compUsing2T.comparator);
-			t*= 2;
-			if(t >= n) break;
+			t *= 2;
+			if (t >= n) break;
 
-			int[] newGroup = new int[n+1];
-			Arrays.fill(newGroup, -1);
+			int[] newGroup = new int[n + 1];
 			newGroup[perm.get(0)] = 0;
+			newGroup[n] = -1;
 
-			for(int i=1; i<n; i++){
-				if(compUsing2T.comparator.compare(perm.get(i-1), perm.get(i)) < 0){
-					newGroup[perm.get(i)] = newGroup[perm.get(i-1)] +1;
-				}else{
-					newGroup[perm.get(i)] = newGroup[perm.get(i-1)];
+			for (int i = 1; i < n; i++) {
+				if (compUsing2T.comparator.compare(perm.get(i - 1), perm.get(i)) < 0) {
+					newGroup[perm.get(i)] = newGroup[perm.get(i - 1)] + 1;
+				} else {
+					newGroup[perm.get(i)] = newGroup[perm.get(i - 1)];
 				}
 			}
 			group = newGroup;
-			compUsing2T.changeValues(t , group);
+			compUsing2T.changeValues(t, group);
 		}
 		return perm;
 	}
 
 	static class CompUsing2T {
+		private int n;
 		private int t;
 		private int[] group;
 
-		public CompUsing2T( int t, int[] group) {
+		public CompUsing2T(int n, int t, int[] group) {
+			this.n = n;
 			this.t = t;
 			this.group = Arrays.copyOf(group, group.length);
 		}
 
-		public void changeValues(int t, int[] group){
+		public void changeValues(int t, int[] group) {
 			this.t = t;
 			this.group = Arrays.copyOf(group, group.length);
 		}
@@ -82,7 +83,14 @@ public class SuffixArray_Manber_Myers {
 				if (group[o1] != group[o2]) {
 					return group[o1] - group[o2];
 				}
-				return group[o1+t] - group[o2+t];
+				int left = o1 + t, right = o2 + t;
+				if (o1 + t > n) {
+					left = n;
+				}
+				if (o2 + t > n) {
+					right = n;
+				}
+				return group[left] - group[right];
 			}
 		};
 	}
