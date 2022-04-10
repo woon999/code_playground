65e02c1c19cbe42960701e71ab92c24a10c4ae05
jongwon <54282927+loosie@users.noreply.github.com>
2022-04-11 00:51:40 +0900

    feat: lcp array update

--- data_structure/src/suffixarray_lcparray/SuffixArray_Sort.java
+++ data_structure/src/suffixarray_lcparray/SuffixArray_Sort.java
@@ -1,8 +1,6 @@
-package suffixarray;
+package suffixarray_lcparray;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
--- data_structure/src/suffixarray_lcparray/SuffixArray_manberMyers_andLCPArray.java
+++ data_structure/src/suffixarray_lcparray/SuffixArray_manberMyers_andLCPArray.java
@@ -1,4 +1,4 @@
-package suffixarray;
+package suffixarray_lcparray;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,19 +7,31 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Suffix Array 멘버-마이어스 알고리즘으로 생성하기
- * while문 내부 시간복잡도 O(nlgn)
- * 배열 lgn번 정렬
- * 전체 시간복잡도 O(n*(lgn)^2)
+ * Suffix Array 멘버-마이어스 알고리즘으로 생성하기 + LCP 배열 만들기
+ * Suffix Array
+ * - while문 내부 시간복잡도 O(nlgn)
+ * - 배열 lgn번 정렬
+ * - 전체 시간복잡도 O(n*(lgn)^2)
+ *
+ * LCP Array
+ * 전체 시간복잡도 O(m + lgn)
  */
-public class SuffixArray_Manber_Myers {
+public class SuffixArray_manberMyers_andLCPArray {
 	public static void main(String[] args) {
 		String text = "banana";
-		List<Integer> suffix = getSuffixArray(text);
-
-		for (int i : suffix) {
-			System.out.println(i + " - " + text.substring(i));
+		List<Integer> sa = getSuffixArray(text);
+		int[] lcp = getLCP(text, sa);
+		for (int i : sa) {
+			// System.out.println(i + " - " + text.substring(i));
+			System.out.print(i + " ");
 		}
+		System.out.println();
+
+		System.out.print("x ");
+		for (int i : lcp) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
 	}
 
 	private static List<Integer> getSuffixArray(String text) {
@@ -27,11 +39,11 @@ public class SuffixArray_Manber_Myers {
 		int t = 1;
 
 		// 접미사 배열이 될 반환 값 (이 동적 배열을 log(n)번 정렬)
-		List<Integer> perm = new ArrayList<>();
+		List<Integer> sa = new ArrayList<>();
 		// group
 		int[] group = new int[n + 1];
 		for (int i = 0; i < n; i++) {
-			perm.add(i);
+			sa.add(i);
 			// text가 영어 소문자로만 이루어졌다고 가정
 			group[i] = text.charAt(i) - 'a';
 		}
@@ -40,25 +52,59 @@ public class SuffixArray_Manber_Myers {
 		CompUsing2T compUsing2T = new CompUsing2T(n, t, group);
 		while (t < n) {
 			// group[]은 첫 t글자를 기준으로 계산해뒀다. 첫 2t글자를 기준으로 perm을 다시 정렬한다.
-			Collections.sort(perm, compUsing2T.comparator);
+			Collections.sort(sa, compUsing2T.comparator);
+
 			t *= 2;
-			if (t >= n) break;
+			if (t >= n)
+				break;
 
 			int[] newGroup = new int[n + 1];
-			newGroup[perm.get(0)] = 0;
+			newGroup[sa.get(0)] = 0;
 			newGroup[n] = -1;
 
 			for (int i = 1; i < n; i++) {
-				if (compUsing2T.comparator.compare(perm.get(i - 1), perm.get(i)) < 0) {
-					newGroup[perm.get(i)] = newGroup[perm.get(i - 1)] + 1;
+				if (compUsing2T.comparator.compare(sa.get(i - 1), sa.get(i)) < 0) {
+					newGroup[sa.get(i)] = newGroup[sa.get(i - 1)] + 1;
 				} else {
-					newGroup[perm.get(i)] = newGroup[perm.get(i - 1)];
+					newGroup[sa.get(i)] = newGroup[sa.get(i - 1)];
 				}
 			}
 			group = newGroup;
 			compUsing2T.changeValues(t, group);
 		}
-		return perm;
+		return sa;
+	}
+
+	static int[] getLCP(String text, List<Integer> sa) {
+		int n = sa.size();
+		int[] lcp = new int[n - 1];
+
+		// sa의 역함수 배열 isa[sa[i]] = i
+		int[] isa = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			isa[sa.get(i)] = i;
+		}
+
+		int k = 0;
+		for (int i = 0; i < n; i++) {
+			int idx = isa[i];
+			if (idx == n - 1)
+				continue;
+
+			int j = sa.get(idx + 1);
+			while (i + k < n && j + k < n) {
+				if (text.charAt(i + k) != text.charAt(j + k))
+					break;
+				k++;
+			}
+
+			lcp[idx] = k;
+			if (k > 0) { // i+1는 최소 k-1
+				k--;
+			}
+		}
+		return lcp;
 	}
 
 	static class CompUsing2T {
