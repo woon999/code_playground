f5415420b5c8d59eaf10147a45bb7dc23646f6dd
jongwon <54282927+loosie@users.noreply.github.com>
2022-06-14 00:27:18 +0900

    hashset: resize 메서드 테스트

--- data_structure/src/hashset/HashSetTester.java
+++ data_structure/src/hashset/HashSetTester.java
@@ -6,10 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class HashSetTester {
 
-	HashSet<Integer> intSet = new HashSet<>();
+
 
 	@Test
 	void addTest(){
+		HashSet<Integer> intSet = new HashSet<>();
 		boolean one = intSet.add(1);
 		boolean two = intSet.add(2);
 		boolean againOne = intSet.add(1);
@@ -17,5 +18,25 @@ public class HashSetTester {
 		assertEquals(one, true);
 		assertEquals(two, true);
 		assertEquals(againOne, false);
+	}
+
+	@Test
+	void resizeTest(){
+		// hashSet 사이즈 10으로 초기화
+		int initialSize = 10;
+		HashSet<Integer> intSet = new HashSet<>(initialSize);
+		for(int i=0; i<7; i++){
+			intSet.add(i);
+		} // 70% 저장
+
+		int tableSize = intSet.table.length;
+		assertEquals(tableSize, initialSize);
+
+		// 75% 초과 resizing
+		intSet.add(8);
+
+		tableSize = intSet.table.length;
+		// table resize -> 2배로 증가
+		assertEquals(tableSize, initialSize*2);
 	}
 }
