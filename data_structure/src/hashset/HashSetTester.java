4337da9528468392b96f29f9b140b25e818f2297
jongwon <54282927+loosie@users.noreply.github.com>
2022-06-14 22:15:31 +0900

    hashset: remove 메서드 테스트

--- data_structure/src/hashset/HashSetTester.java
+++ data_structure/src/hashset/HashSetTester.java
@@ -8,11 +8,13 @@ public class HashSetTester {
 
 	@Test
 	void addTest(){
+		// given, when
 		HashSet<Integer> intSet = new HashSet<>();
 		boolean one = intSet.add(1);
 		boolean two = intSet.add(2);
 		boolean againOne = intSet.add(1);
 
+		// then
 		assertEquals(one, true);
 		assertEquals(two, true);
 		assertEquals(againOne, false);
@@ -20,7 +22,7 @@ public class HashSetTester {
 
 	@Test
 	void resizeTest(){
-		// hashSet 사이즈 10으로 초기화
+		// given. hashSet 사이즈 10으로 초기화
 		int initialSize = 10;
 		HashSet<Integer> intSet = new HashSet<>(initialSize);
 		for(int i=0; i<7; i++){
@@ -30,11 +32,31 @@ public class HashSetTester {
 		int tableSize = intSet.table.length;
 		assertEquals(tableSize, initialSize);
 
-		// 75% 초과 resizing
+		// when. 75% 초과 resizing
 		intSet.add(8);
 
+		// then
 		tableSize = intSet.table.length;
 		// table resize -> 2배로 증가
 		assertEquals(tableSize, initialSize*2);
+	}
+
+	@Test
+	void removeTest(){
+		HashSet<Integer> intSet = new HashSet<>();
+		// given
+		boolean one = intSet.add(1);
+		boolean two = intSet.add(2);
+
+		assertEquals(one, true);
+		assertEquals(two, true);
+		// assertEquals(intSet.contains(1), true);
+
+		// when
+		boolean removeOne = intSet.remove(1);
+
+		// then
+		assertEquals(removeOne, true);
+		// assertEquals(intSet.contains(1), false);
 	}
 }
