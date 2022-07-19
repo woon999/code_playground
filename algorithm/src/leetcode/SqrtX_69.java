870696fd2f5ec58f924400c4f00c73b1944db376
jongwon <54282927+loosie@users.noreply.github.com>
2022-07-20 07:56:17 +0900

    leet - (69. Sqrt X)

---
+++ algorithm/src/leetcode/SqrtX_69.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+// #69 - SqrtX
+
+public class SqrtX_69 {
+	public static void main(String[] args) {
+		int x = 8;
+		System.out.println("mySqrt = " + mySqrt(x));
+	}
+
+	public static int mySqrt(int x) {
+		return (int)Math.sqrt(x);
+	}
+}
