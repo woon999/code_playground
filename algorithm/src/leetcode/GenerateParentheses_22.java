dbeeefd598e31c8b37d90af07cde2c6bb2aef71e
jongwon <54282927+loosie@users.noreply.github.com>
2022-07-24 14:58:53 +0900

    fix: leet - (22. Generate Parentheses)

--- algorithm/src/leetcode/GenerateParentheses_22.java
+++ algorithm/src/leetcode/GenerateParentheses_22.java
@@ -14,35 +14,29 @@ public class GenerateParentheses_22 {
 
 	public static List<String> generateParenthesis(int n) {
 		List<String> answer = new ArrayList<>();
-		make(1, 1, 2*n, new StringBuilder(), answer);
+		StringBuilder sb = new StringBuilder();
+		make(1, 1, 2*n, sb.append('('), answer);
 		return answer;
 	}
 
 	static void make(int open, int depth, int limit, StringBuilder sb, List<String> answer) {
-		if (depth == 1) { // start
-			sb.append('(');
-		}
-
 		if (open < 0) {
 			return;
 		}
 
 		if (depth == limit && open == 0) {
-			System.out.println(sb.toString());
 			answer.add(sb.toString());
 			return;
 		}
 
 		if (depth < limit) {
-			StringBuilder left = new StringBuilder(sb);
-			left.append(')');
-			make(open - 1, depth + 1, limit, left, answer);
-		}
+			StringBuilder addBuilder = new StringBuilder(sb);
+			addBuilder.append(')');
+			make(open - 1, depth + 1, limit, addBuilder, answer);
 
-		if (depth < limit) {
-			StringBuilder right = new StringBuilder(sb);
-			right.append('(');
-			make(open + 1, depth + 1, limit, right, answer);
+			addBuilder.deleteCharAt(addBuilder.length()-1);
+			addBuilder.append('(');
+			make(open + 1, depth + 1, limit, addBuilder, answer);
 		}
 	}
 
