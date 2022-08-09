dfe8003a63b90c32c84d304a526d0895f84e2d3b
jongwon <54282927+loosie@users.noreply.github.com>
2022-08-10 00:04:16 +0900

    leet - (136. Single Number) #2

--- algorithm/src/leetcode/SingleNumber_136.java
+++ algorithm/src/leetcode/SingleNumber_136.java
@@ -5,17 +5,34 @@ package leetcode;
 // wow wow!!
 // ref: https://leetcode.com/problems/single-number/discuss/1771771/Think-it-through-oror-Time%3A-O(n)-Space%3A-O(1)-oror-Python-Explained
 
+import java.util.Arrays;
+
 public class SingleNumber_136 {
 	public static void main(String[] args) {
-		int[] nums = {4,1,2,1,2};
+		int[] nums = {4, 4, 1, 3, 2, 1, 2};
 		System.out.println("singleNumber(nums) = " + singleNumber(nums));
 	}
 
-	public static int singleNumber(int[] nums) {
+	public static int singleNumber_wow(int[] nums) {
 		int xor = 0;
-		for(int num : nums){
+		for (int num : nums) {
 			xor ^= num;
 		}
 		return xor;
+	}
+
+	public static int singleNumber(int[] nums) {
+		Arrays.sort(nums);
+		int i = 0;
+		for (i = 0; i < nums.length - 1; i += 2) {
+			if (nums[i] != nums[i + 1]) {
+				break;
+			}
+		}
+
+		if (i == nums.length - 1) {
+			return nums[nums.length - 1];
+		}
+		return nums[i];
 	}
 }
