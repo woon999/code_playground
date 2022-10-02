7be2978a48716c51674b3077065331413a37267f
jongwon <54282927+loosie@users.noreply.github.com>
2022-10-02 17:17:48 +0900

    leet java (1. Two Sum) improvement

--- algorithm/src/leetcode/TwoSum_1.java
+++ algorithm/src/leetcode/TwoSum_1.java
@@ -12,9 +12,10 @@ public class TwoSum_1 {
 		int[] nums = {3, 2, 4};
 		int target = 6;
 		
-		System.out.println(Arrays.toString(twoSum(nums, target)));
+		System.out.println(Arrays.toString(twoSum_improvement(nums, target)));
 	}
-	
+
+	// O(n^2)
 	public static int[] twoSum(int[] nums, int target) {
 		int n = nums.length;
 		for(int i=0; i<n; i++) {
@@ -28,5 +29,19 @@ public class TwoSum_1 {
 		
 		return new int[] {0,1};
     }
+
+	// O(n)
+	public static int[] twoSum_improvement(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i=0; i<nums.length; i++){
+			int num = nums[i];
+			if(map.containsKey(target - num)){
+				return new int[]{map.get(target - num), i};
+			}
+			map.put(num, i);
+		}
+
+		return new int[] {0,1};
+	}
 }
 
