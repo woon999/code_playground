4cba59ca8d4d59fe09ebf0c78f9b57039c6c435b
jongwon <54282927+loosie@users.noreply.github.com>
2022-11-25 23:08:16 +0900

    leet ruby (344. Reverse String)

---
+++ algorithm/src/leetcode/ruby/ReverseString_344.rb
@@ -0,0 +1,10 @@
+# 344 Reverse String
+
+# @param {Character[]} s
+# @return {Void} Do not return anything, modify s in-place instead.
+def reverse_string(s)
+  s.reverse!
+end
+
+s = ["h","e","l","l","o"]
+p reverse_string s
