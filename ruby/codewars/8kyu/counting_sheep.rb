9a999808000deb3b602578ff9a0dd7dd94bafa90
charleslee <jong9712@naver.com>
2023-05-21 20:35:20 +0900

    ruby (codewars 8kyu) counting sheep

---
+++ ruby/codewars/8kyu/counting_sheep.rb
@@ -0,0 +1,12 @@
+def countSheeps array
+  array.count(true)
+end
+
+array1 = [true,  true,  true,  false,
+          true,  true,  true,  true ,
+          true,  false, true,  false,
+          true,  false, false, true ,
+          true,  true,  true,  true ,
+          false, false, true,  true ]
+
+p countSheeps(array1) == 17
