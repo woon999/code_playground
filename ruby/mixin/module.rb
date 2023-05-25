989dfcccf90d5dfae7e3ae1fe86418ad1c7e30a4
charleslee <jong9712@naver.com>
2023-05-25 17:50:27 +0900

    ruby ancestors

--- ruby/mixin/module.rb
+++ ruby/mixin/module.rb
@@ -41,3 +41,34 @@ p My.new.instance_method
 
 # p My.module_method
 # /ruby/mixin/module.rb:38:in `<main>': undefined method `module_method' for My:Class (NoMethodError)
+
+=begin
+
+루비에서는 클래스가 생성될 때 **ancestors** 배열에 클래스 조상의 목록을 저장해둡니다.
+ancestors에는 이 클래스가 상속받는 모든 클래스, 자기 자신, 그리고 `include`와 `prepend`를 통해 mixin된 모듈들이 포함됩니다.
+
+클래스의 인스턴스 메서드를 호출하면 ancestors 배열의 앞에서부터 메서드 정의를 찾습니다.
+상속 개념과 유사하게, 메서드 정의를 찾지 못하면 다음 조상에게서 찾는 식입니다.
+즉 둘 이상의 선조들이 같은 이름의 메서드를 정의하고 있다면 더 가까운 선조에 정의된 메서드를 실행합니다.
+참고로 `BasicObject`까지 거슬러 올라갔는데도 메서드를 찾지 못하면
+  [BasicObject#method_missing](https://ruby-doc.org/core-2.5.0/BasicObject.html#method-i-method_missing)이 실행되는데, 몇몇 루비 gem들은 이를 이용해 이해하기 쉽지 않은 흑마술을 부리기도 합니다.
+
+=end
+
+p String.ancestors
+# [String, Comparable, Object, Kernel, BasicObject]
+
+p My.ancestors
+# [My, MyModule, Object, Kernel, BasicObject]
+
+p "foo".upcase
+# "FOO"
+
+p "foo".object_id
+# 60
+
+p String.instance_method(:upcase)
+# #<UnboundMethod: String#upcase(*)>
+
+p String.instance_method(:object_id)
+# #<UnboundMethod: Kernel#object_id()>
