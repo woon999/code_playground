989209fbaabaaf2affa9e9febef3a2752a48de6a
jongwon <54282927+loosie@users.noreply.github.com>
2023-05-31 23:52:45 +0900

    c++ compiler - 어휘분석

---
+++ c++/compiler/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Scanner.cpp"
+// #include "Token.h"
+
+using namespace std;
+using std::string;
+using std::vector;
+
+auto scan(string) -> vector<Token>;
+auto printTokenList(vector<Token> tokenList) -> void
+{
+    cout << setw(12) << left << "KIND"
+         << "STRING" << endl;
+    cout << string(23, '-') << endl;
+    for (auto &token : tokenList)
+        cout << token << endl;
+}
+
+auto main() -> int
+{
+    string sourceCode = R""""(
+        function main() {
+            print 'Hello, World!';
+        }
+    )"""";
+
+    cout << sourceCode << endl;
+    printTokenList(scan(sourceCode));
+
+    return 1;
+}
