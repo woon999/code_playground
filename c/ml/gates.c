8a7a55adb993b34c56b446d44344330579bee26d
charleslee <jong9712@naver.com>
2023-06-08 18:05:02 +0900

    c ml - other gates

--- c/ml/gates.c
+++ c/ml/gates.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <math.h>
 
+typedef float sample[3];
+
+
 // 로지스틱 회귀분석에서는 종속변수 y값이 0 또는 1을 갖는다. 
 // 그래서 우리는 주어진 데이터를 분류할 때 0인지 1인지 예측하는 모델을 만들어야 한다.
 float sigmoidf(float x){
@@ -10,14 +13,31 @@ float sigmoidf(float x){
 }
 
 // OR-gate
-float train[][3] = {
+float or_train[][3] = {
     {0, 0, 0},
     {1, 0, 1},
     {0, 1, 1},
     {1, 1, 1},
 };
 
-#define train_count (sizeof(train)/sizeof(train[0]))
+// AND-gate
+float and_train[][3] = {
+    {0, 0, 0},
+    {1, 0, 0},
+    {0, 1, 0},
+    {1, 1, 1},
+};
+
+// NAND-gate
+float nand_train[][3] = {
+    {0, 0, 1},
+    {1, 0, 1},
+    {0, 1, 1},
+    {1, 1, 0},
+};
+
+sample *train = nand_train;
+size_t train_count = 4;
 
 float rand_float(void)
 {
@@ -33,8 +53,8 @@ float cost(float w1, float w2, float b)
         float x2 = train[i][1];
         float y = sigmoidf(x1*w1 + x2*w2 + b); // O or 1?
 
-        float diff = y - train[i][2];
-        result += diff*diff;
+        float d = y - train[i][2];
+        result += d*d;
     }
 
     return result/train_count;
@@ -43,13 +63,15 @@ float cost(float w1, float w2, float b)
 int main()
 {
     srand(time(NULL));
-    float w1 = rand_float()*10.f;
-    float w2 = rand_float()*10.f;
-    float b = rand_float()*5.f;
-    
+    rand();
+    float w1 = rand_float();
+    float w2 = rand_float();
+    float b = rand_float();
+
+    printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
     float eps = 1e-1;
     float rate = 1e-1;
-    for(size_t i=0; i<10000; ++i){
+    for(size_t i=0; i<100000; ++i){
         float c = cost(w1, w2, b);
         // printf("%f\n", cost(w1, w2, b));
         float dw1 = (cost(w1+eps, w2, b) - c)/eps;
@@ -62,6 +84,14 @@ int main()
     }
 
     printf("w1 = %f, w2 = %f, b = %f, c = %f\n", w1, w2, b, cost(w1, w2, b));
+
+    for(size_t i=0; i<2; ++i){
+        for(size_t j=0; j<2; ++j){
+            printf("%zu | %zu = %f \n", i, j, sigmoidf(i*w1 + j*w2 + b));
+        }
+    }
+
+    printf("sig: %f", sigmoidf(w1 + w2 + b));
 
     return 0;
 }
