4dd5103d8ed79ee8b0999c297258900564e03765
jongwon <54282927+loosie@users.noreply.github.com>
2023-06-10 20:43:17 +0900

    ruby (codewars 8kyu) string to number

---
+++ ruby/codewars/8kyu/convert_a_string_to_a_number.rb
@@ -0,0 +1,12 @@
+# https://www.codewars.com/kata/544675c6f971f7399a000e79/train/ruby
+
+def string_to_number(s)
+  s.to_i
+end
+
+
+
+p string_to_number "1234" # 1234
+p string_to_number "605"  # 605
+p string_to_number "1405" # 1405
+p string_to_number "-7"   # -7
