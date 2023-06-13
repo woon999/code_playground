282b19f3ad71bc9e636916b72d9414a0fd9f342e
charleslee <jong9712@naver.com>
2023-06-13 11:23:02 +0900

    ruby (codewars 8kyu) string repeat

---
+++ ruby/codewars/8kyu/string_repeat.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/train/ruby
+
+def repeat_str (n, s)
+  s * n
+end
+
+p repeat_str(3, "*") # "***"
+p repeat_str(5, "#") # "#####"
+p repeat_str(2, "ha ") # "ha ha "
