260d5a66c874653689535e554aec3277103d2ac6
jongwon <54282927+loosie@users.noreply.github.com>
2023-06-23 00:28:56 +0900

    create linux package

--- ruby/fiber_and_multi_thread/fiber.rb
+++ ruby/fiber_and_multi_thread/fiber.rb
@@ -8,14 +8,14 @@
 
 require 'benchmark'
 
-counts = Hash.new(0)
-File.foreach("testfile") do |line|
-  line.scan(/\w+/) do |word|
-    word = word.downcase
-    counts[word] += 1
-  end
-end
-counts.keys.sort.each {|k| print "#{k}:#{counts[k]} "}
+# counts = Hash.new(0)
+# File.foreach("testfile") do |line|
+#   line.scan(/\w+/) do |word|
+#     word = word.downcase
+#     counts[word] += 1
+#   end
+# end
+# counts.keys.sort.each {|k| print "#{k}:#{counts[k]} "}
 
 
 puts "\n---------------------------------"
