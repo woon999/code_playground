aa36e24c5f30c6eb82b392b396c80958c550cc92
jongwon <54282927+loosie@users.noreply.github.com>
2023-06-29 22:05:28 +0900

    elixir (codewars 8kyu) beginner series #2 clock

---
+++ elixir/8kyu/beginner_serires.exs
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/elixir
+
+defmodule Kata do
+  def past(h, m, s) do
+    (h * 3600 + m * 60 + s) * 1000
+  end
+end
+
+# Test
+IO.puts Kata.past(0,1,1) == 61000
+IO.puts Kata.past(1,1,1) == 3661000
+IO.puts Kata.past(0,0,0) == 0
+IO.puts Kata.past(1,0,1) == 3601000
+IO.puts Kata.past(1,0,0) == 3600000
