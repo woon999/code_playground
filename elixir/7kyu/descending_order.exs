c8c1c820925c9285ded7ff47f88eaa64fc3135dd
jongwon <54282927+loosie@users.noreply.github.com>
2023-07-08 21:39:23 +0900

    elixir (codewars 7kyu) descending order

--- elixir/7kyu/descending_order.exs
+++ elixir/7kyu/descending_order.exs
@@ -12,7 +12,6 @@ end
 # Test
 IO.puts Kata.descending_order(0) == 0
 IO.puts Kata.descending_order(1) == 1
-IO.puts Kata.descending_order(123456789)
 IO.puts Kata.descending_order(123456789) == 987654321
 IO.puts Kata.descending_order(145263) == 654321
 IO.puts Kata.descending_order(1254859723) == 9875543221
