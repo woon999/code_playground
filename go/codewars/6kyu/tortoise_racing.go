dbfea7bb0dece87a6d4346e10149dce8e06fbe71
jongwon <54282927+loosie@users.noreply.github.com>
2023-09-08 20:57:57 +0900

    go (codewars 6kyu) tortoise racing

--- go/codewars/6kyu/tortoise_racing.go
+++ go/codewars/6kyu/tortoise_racing.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func Race(v1, v2, g int) [3]int {
+	if v1 >= v2 {
+		return [3]int{-1, -1, -1}
+	}
+	t := float64(g) / float64(v2-v1) * 3600
+	return [3]int{int(t / 3600), int(t/60) % 60, int(t) % 60}
+}
+
+func main() {
+	fmt.Println(Race(720, 850, 70)) // [3]int{0, 32, 18}
+}
