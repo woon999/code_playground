f4744489a77bf0ff7387ff9f4e23fdd8490e9b93
jongwon <54282927+loosie@users.noreply.github.com>
2023-10-20 21:43:39 +0900

    go (codewars 8kyu) compare within margin

--- go/codewars/8kyu/compare_within_margin.go
+++ go/codewars/8kyu/compare_within_margin.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func CloseCompare(a, b, margin float64) int {
-	if a == b || (a > b && a-b < margin) || (b > a && b-a < margin) {
+	if math.Abs(a-b) <= margin {
 		return 0
-	} else if a > b {
-		return 1
-	} else {
+	} else if a < b {
 		return -1
+	} else {
+		return 1
 	}
 }
 
 func main() {
 	fmt.Println(CloseCompare(4, 5, 0) == -1)
+	fmt.Println(CloseCompare(4, 5, 3) == 0)
 }
