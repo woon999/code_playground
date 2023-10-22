c505275b93648628cec2f8511b03ba5b402019b9
jongwon <54282927+loosie@users.noreply.github.com>
2023-10-22 12:51:25 +0900

    go (codewars 8kyu) find nearest square number

--- go/codewars/8kyu/find_nearest_square_number.go
+++ go/codewars/8kyu/find_nearest_square_number.go
@@ -1,16 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func NearestSq(n int) int {
-	var i int
-	for i = 1; i*i < n; i++ {
-	}
-	if (i*i - n) < (n - (i-1)*(i-1)) {
-		return i * i
-	} else {
-		return (i - 1) * (i - 1)
-	}
+	//var i int
+	//for i = 1; i*i < n; i++ {
+	//}
+	//if (i*i - n) < (n - (i-1)*(i-1)) {
+	//	return i * i
+	//} else {
+	//	return (i - 1) * (i - 1)
+	//}
+
+	n = int(math.Round(math.Sqrt(float64(n))))
+
+	return n * n
 }
 
 func main() {
