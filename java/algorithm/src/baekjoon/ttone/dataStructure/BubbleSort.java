c87c4a57c92d19d3f780d0e73ede1aa6f1a9a78f
jongwon <54282927+loosie@users.noreply.github.com>
2024-01-24 00:04:14 +0900

    bugfix: boj #1517 dataStructure (1517. 버블 소트) - 세그먼트 트리

--- java/algorithm/src/baekjoon/ttone/dataStructure/BubbleSort.java
+++ java/algorithm/src/baekjoon/ttone/dataStructure/BubbleSort.java
@@ -2,37 +2,37 @@ package baekjoon.ttone.dataStructure;
 
 // #1517 dataStructure (1517. 버블 소트) - 세그먼트 트리 
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BubbleSort {
 
 	static int n;
-	static long[] elements, index, tree;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		n = Integer.parseInt(br.readLine());
-		elements = new long[n];
+		long[] elements = new long[n];
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i=0; i<n; i++) {
 			elements[i] = Integer.parseInt(st.nextToken());
 		}
-		Map<Long, Integer> pos = new HashMap<>();
-		for(int i=0; i<n; i++) {
-			pos.put(elements[i], i);
+
+		// <K, V> = <segment tree index, elements in queue (중복 값 처리)>
+		Map<Long, Queue<Integer>> pos = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			pos.computeIfAbsent(elements[i], k -> new LinkedList<>()).offer(i);
 		}
-		index = elements.clone();
+
+		long[] index = elements.clone();
 		Arrays.sort(index);
-		
-		tree = new long[getTreeSize()];
+
+		long[] tree = new long[getTreeSize()];
 		long ans =0;
+		System.out.println("------------------");
 		for(int i=0; i<n; i++)	{
-			int idx = pos.get(index[i]);
-			ans += sum(0, n-1, 1, idx+1, n-1);
-			update(0, n-1, 1, idx, 1);
+			int idx = pos.get(index[i]).poll();
+			ans += sum(tree,0, n-1, 1, idx+1, n-1);
+			update(tree, 0, n-1, 1, idx, 1);
 		}
 		System.out.println(ans);
 	}
@@ -42,7 +42,7 @@ public class BubbleSort {
 		return (int)Math.pow(2, h);
 	}
 	
-	static long sum(int start, int end, int node, int left, int right) {
+	static long sum(long[] tree, int start, int end, int node, int left, int right) {
 		if(end < left || right < start ) return 0;
 		if(left <= start && end <= right) {
 			return tree[node];
@@ -50,17 +50,17 @@ public class BubbleSort {
 		}
 		
 		int mid = (start+end)/2;
-		return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
+		return sum(tree, start, mid, node*2, left, right) + sum(tree, mid+1, end, node*2+1, left, right);
 	}
 	
-	static void update(int start, int end, int node, int idx, int dif) {
+	static void update(long[] tree, int start, int end, int node, int idx, int dif) {
 		if(start == end ) {
 			tree[node] = dif;
 			return;
 		}
 		int mid = (start+end)/2;
-		if(idx <= mid) update(start, mid, node*2, idx, dif);
-		else update(mid+1, end, node*2+1, idx, dif);
+		if(idx <= mid) update(tree, start, mid, node*2, idx, dif);
+		else update(tree, mid+1, end, node*2+1, idx, dif);
 		
 		tree[node] = tree[node*2]+tree[node*2+1];
 	}
