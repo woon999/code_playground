4f4995eb76775e88f0ba8c10cd3cb42cc3cb4480
jongwon <54282927+loosie@users.noreply.github.com>
2024-05-18 00:35:20 +0900

    #

--- rust/web/axum-basic-web/src/error.rs
+++ rust/web/axum-basic-web/src/error.rs
@@ -3,7 +3,7 @@ use serde::Serialize;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Clone, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
 #[serde(tag = "type", content = "data")]
 pub enum Error {
     LoginFail,
